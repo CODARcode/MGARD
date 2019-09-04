9847eee6440d4316d566143dfc67fc0aa52b3b37
Ben Whitney <whitneybe@ornl.gov>
2019-09-04 15:04:42 -0400

    Add tests for entity measure functions.

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 #include "tests.hpp"
+#include "test_measure.hpp"
 
 int main() {
+    std::cout << "TESTING MEASURE ..." << std::endl;
+    test_measure();
+
     std::cout << "PASSED ALL TESTS" << std::endl;
     return 0;
 }
