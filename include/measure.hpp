3897c29447337cef238a84d589c64a0367691a49
Ben Whitney <whitneybe@ornl.gov>
2019-09-05 10:55:01 -0400

    Document entity measure functions.

--- include/measure.hpp
+++ include/measure.hpp
@@ -1,30 +1,63 @@
 #ifndef MEASURE_HPP
 #define MEASURE_HPP
+//!\file
+//!\brief Functions to compute measures of elements.
+//!
+//!See §3 of [these notes][LNoGR] for more on the formulae used here.
+//!
+//![LNoGR]: https://people.eecs.berkeley.edu/~jrs/meshpapers/robnotes.pdf
 
 #include <cstddef>
 
 namespace helpers {
 
+//!Find the Euclidean inner product of two vectors.
+//!
+//!\param [in] a First vector, of size `D`.
+//!\param [in] b Second vector, of size `D`.
+//!
+//!\return Euclidean inner product of the vectors.
 template <std::size_t D>
 double inner_product(double const * const a, double const * const b);
 
+//!Find the Euclidean norm of a vector.
+//!
+//!\param [in] a Vector, of size `D`.
+//!
+//!\return Euclidean norm of the vector.
 template <std::size_t D>
 double norm(double const * const a);
 
+//!Subtract one vector from another.
+//!
+//!\param [in] a Subtrahend, of size `D`.
+//!\param [in] b Minuend, of size `D`.
+//!\param [out] c Difference, of size `D`.
 template <std::size_t D>
 void subtract_into(
     double const * const a,
     double const * const b,
-    double const * const c
+    double * const c
 );
 
-//See §2 of <https://people.eecs.berkeley.edu/~jrs/meshpapers/robnotes.pdf>.
+//!Find the signed area of a parallelogram.
+//!
+//!\param [in] a, b, c Coordinates, of size 2, of three vertices of the
+//!parallelogram, with the third lying between the first and second.
+//!
+//!\return Signed area of the parallelogram.
 double orient_2d(
     double const * const a,
     double const * const b,
     double const * const c
 );
 
+//!Find the signed area of a parallelepiped.
+//!
+//!\param [in] a, b, c, d Coordinates, of size 3, of four vertices of the
+//!parallelepiped, with the fourth sharing edges with each of the first three.
+//!
+//!\return Signed volume of the parallelepiped.
 double orient_3d(
     double const * const a,
     double const * const b,
@@ -32,10 +65,28 @@ double orient_3d(
     double const * const d
 );
 
+//!Find the length of an edge.
+//!
+//!\param [in] p Coordinates of the vertices of the edge (six values in total,
+//!grouped by vertex).
+//!
+//!\return Length of the edge.
 double edge_measure(double const * const p);
 
+//!Find the area of a triangle.
+//!
+//!\param[in] p Coordinates of the vertices of the triangle (nine values in
+//!total, grouped by vertex).
+//!
+//!\return Area of the triangle.
 double tri_measure(double const * const p);
 
+//!Find the volume of a tetrahedron.
+//!
+//!\param [in] p Coordinates of the vertices of the tetrahedron (twelve values
+//!in total, grouped by vertex).
+//!
+//!\return Volume of the tetrahedron.
 double tet_measure(double const * const p);
 
 }
