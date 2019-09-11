8d502e05bf8a6d9a7fbf84835916699388e2898c
tugluk <user@yaban>
2019-09-11 16:10:12 -0400

    Initial qoi cpp file

---
+++ src/mgard_qoi.cpp
@@ -0,0 +1,62 @@
+// Copyright 2017, Brown University, Providence, RI.
+//
+//                         All Rights Reserved
+//
+// Permission to use, copy, modify, and distribute this software and
+// its documentation for any purpose other than its incorporation into a
+// commercial product or service is hereby granted without fee, provided
+// that the above copyright notice appear in all copies and that both
+// that copyright notice and this permission notice appear in supporting
+// documentation, and that the name of Brown University not be used in
+// advertising or publicity pertaining to distribution of the software
+// without specific, written prior permission.
+//
+// BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+// INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR ANY
+// PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY BE LIABLE FOR
+// ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+//
+//
+// MGARD: MultiGrid Adaptive Reduction of Data
+// Authors: Mark Ainsworth, Ozan Tugluk, Ben Whitney
+// Corresponding Author: Ozan Tugluk
+//
+// version: 0.0.0.2
+//
+// This file is part of MGARD.
+//
+// MGARD is distributed under the OSI-approved Apache License, Version 2.0.
+// See accompanying file Copyright.txt for details.
+//
+
+
+#include "mgard_qoi.h"
+
+
+namespace mgard_qoi
+{
+
+
+  double qoi_ave(const int nrow, const int ncol, const int nfib, std::vector<double> u)
+  {
+    double  sum = 0;
+    
+    for ( double x : u ) sum += x;
+    
+    return sum/u.size();
+  }
+  
+  float qoi_ave(const int nrow, const int ncol, const int nfib, std::vector<float> u)
+  {
+   float  sum = 0;
+    
+    for ( double x : u ) sum += x;
+    
+    return sum/u.size();
+  }
+
+  
+}
