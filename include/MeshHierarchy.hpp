e510330cf066fd6044380f449bc45957c46b6c01
Ben Whitney <whitneybe@ornl.gov>
2019-09-20 16:27:01 -0400

    Add base class for mesh hierarchies.

---
+++ include/MeshHierarchy.hpp
@@ -0,0 +1,276 @@
+#ifndef MESHHIERARCHY_HPP
+#define MESHHIERARCHY_HPP
+
+#include <cstddef>
+
+#include <vector>
+
+#include "MeshLevel.hpp"
+
+namespace mgard {
+
+//I would like to allow `u` in `{de,re}compose` to be something other than
+//`double *.` `double *` seems to force certain assumptions.
+//    1. The nodal values are contiguous.
+//    2. The nodal values have the same order as the nodes.
+//    3. The nodal values corresponding to coarser levels are grouped together,
+//       and probably at the start of `u`.
+//We may well want to violate these assumptions. For example, in the structured
+//1D case the nodal values had the same order as the nodes but the values
+//corresponding to coarser levels were separated by stride 2^n. Something to
+//return to if necessary.
+
+typedef MeshLevel (*MeshRefiner)(const MeshLevel &);
+
+class MeshHierarchy {
+    public:
+        MeshHierarchy(const std::vector<MeshLevel> &meshes);
+
+        //!Report the number of degrees of freedom in the finest MeshLevel.
+        std::size_t ndof() const;
+
+        //!Transform from nodal coefficients to multilevel coefficients.
+        //!
+        //!\param [in, out] u Nodal values of the input function.
+        //!\param [in] buffer Scratch space.
+        moab::ErrorCode decompose(double * const u, void *buffer = NULL);
+
+        //!Transform from multilevel coefficients to nodal coefficients.
+        //!
+        //!\param [in, out] u Multilevel coefficients of the input function.
+        //!\param [in] buffer Scratch space.
+        moab::ErrorCode recompose(double * const u, void *buffer = NULL);
+
+        //!Report the amount of scratch space (in bytes) needed for all
+        //!hierarchy operations.
+        std::size_t scratch_space_needed() const;
+
+    protected:
+        MeshHierarchy(
+            const MeshLevel &mesh,
+            const MeshRefiner refiner,
+            const std::size_t L
+        );
+
+        //!MeshLevels in the hierarchy, ordered from coarsest to finest.
+        std::vector<MeshLevel> meshes;
+
+        //!Index of finest MeshLevel.
+        std::size_t L;
+
+        //!Scratch space for use in hierarchy operations if no external buffer
+        //if provided.
+        std::vector<char> scratch_space;
+
+        //!Report the number of degrees of freedom in a MeshLevel.
+        //!
+        //!\param l Index of the MeshLevel.
+        std::size_t ndof(const std::size_t l) const;
+
+        //!Report the amount of scratch space (in bytes) needed for
+        //!decomposition.
+        std::size_t scratch_space_needed_for_decomposition() const;
+
+        //!Report the amount of scratch space (in bytes) needed for
+        //!recomposition.
+        std::size_t scratch_space_needed_for_recomposition() const;
+
+        //!Transform from nodal coefficients to multilevel coefficients,
+        //!starting at a given level.
+        //!
+        //!\param [in, out] u Nodal values of the input function.
+        //!\param [in] l Index of the MeshLevel to start at.
+        //!\param [in] buffer Scratch space.
+        moab::ErrorCode decompose(
+            double * const u, const std::size_t l, void * const buffer
+        ) const;
+
+        //!Transform from multilevel coefficients to nodal coefficients,
+        //!starting at a given level.
+        //!
+        //!\param [in, out] u Multilevel coefficients of the input function.
+        //!\param [in] l Index of the MeshLevel to start at. Must be nonzero!
+        //!\param [in] buffer Scratch space.
+        moab::ErrorCode recompose(
+            double * const u, const std::size_t l, void * const buffer
+        ) const;
+
+        //!Project a multilevel component onto the next coarser level.
+        //!
+        //!\param [in] u Nodal values of the input function.
+        //!\param [in] l Index of the MeshLevel
+        //!\param [out] correction Nodal values of the projection.
+        //!\param [in] buffer Scratch space.
+        moab::ErrorCode calculate_correction_from_multilevel_component(
+            double const * const u,
+            const std::size_t l,
+            double * const correction,
+            void * const buffer
+        ) const;
+
+        //!Apply the mass matrix on the coarser level to a multilevel component.
+        //!
+        //!This achieves the same result as applying the mass matrix on the
+        //finer level and restricting back down.
+        //!
+        //!\param [in] u Nodal values of the input function.
+        //!\param [in] l Index of the MeshLevel.
+        //!\param [out] b Matrix–vector product.
+        moab::ErrorCode apply_mass_matrix_to_multilevel_component(
+            double const * const u, const std::size_t l, double * const b
+        ) const;
+
+        //!Interpolate the 'old' values onto the 'new' nodes and subtract.
+        //!
+        //!\param [in, out] Nodal values of the input function.
+        //!\param [in] l Index of the MeshLevel.
+        moab::ErrorCode subtract_interpolant_from_coarser_level_from_new_values(
+            double * const u, const std::size_t l
+        ) const;
+
+        //!Interpolate the 'old' values onto the 'new' nodes and add.
+        //!
+        //!\param [in, out] Nodal values of the input function.
+        //!\param [in] l Index of the MeshLevel.
+        moab::ErrorCode add_interpolant_from_coarser_level_to_new_values(
+            double * const u, const std::size_t l
+        ) const;
+
+        //!Add the correction to the values on the 'old' nodes.
+        //!
+        //!\param [in, out] u Nodal values of the input function.
+        //!\param [in] Index of the MeshLevel.
+        //!\param [in] correction Nodal values of the correction.
+        moab::ErrorCode add_correction_to_old_values(
+            double * const u,
+            const std::size_t l,
+            double const * const correction
+        ) const;
+
+        //!Subtract the correction from the values on the 'old' nodes.
+        //!
+        //!\param [in, out] u Nodal values of the input function.
+        //!\param [in] Index of the MeshLevel.
+        //!\param [in] correction Nodal values of the correction.
+        moab::ErrorCode subtract_correction_from_old_values(
+            double * const u,
+            const std::size_t l,
+            double const * const correction
+        ) const;
+
+        //!Find the handle of a node in a finer mesh.
+        //!
+        //!\param node Handle of the node in the coarse mesh.
+        //!\param l Index of the coarse mesh.
+        //!\param m Index of the fine mesh.
+        //!
+        //!\return Handle of the node in the finest mesh.
+        moab::EntityHandle replica(
+            const moab::EntityHandle node,
+            const std::size_t l,
+            const std::size_t m
+        ) const;
+
+        //!Find the elements obtained by refining a given element.
+        //!
+        //!\param t Handle of the 'parent' element in the coarse mesh.
+        //!\param l Index of the coarse mesh.
+        //!\param m Index of the fine mesh produced by refining the coarse mesh.
+        //!
+        //!\return Handles of the 'child' elements in the finest mesh.
+        moab::Range get_children(
+            const moab::EntityHandle t,
+            const std::size_t l,
+            const std::size_t m
+        ) const;
+
+        //!Determine whether a node is 'new' to a mesh in the hierarchy.
+        //!
+        //!\param node Handle of the node.
+        //\!param l Index of the mesh.
+        bool is_new_node(moab::EntityHandle node, const std::size_t l) const;
+
+        //!Find the measure of an entity of a mesh in the hierarchy.
+        //!
+        //!\param handle Handle of the entity.
+        //!\param l Index of the mesh.
+        double measure(moab::EntityHandle handle, const std::size_t l) const;
+
+        void check_mesh_index_bounds(const std::size_t l) const;
+
+        void check_mesh_indices_nondecreasing(
+            const std::size_t l, const std::size_t m
+        ) const;
+
+        void check_mesh_index_nonzero(const std::size_t l) const;
+
+    private:
+        virtual std::size_t do_ndof(const std::size_t l) const;
+
+        virtual std::size_t do_scratch_space_needed() const;
+
+        virtual
+        std::size_t do_scratch_space_needed_for_decomposition() const = 0;
+
+        virtual
+        std::size_t do_scratch_space_needed_for_recomposition() const = 0;
+
+        virtual moab::EntityHandle do_replica(
+            const moab::EntityHandle node,
+            const std::size_t l,
+            const std::size_t m
+        ) const = 0;
+
+        virtual moab::Range do_get_children(
+            const moab::EntityHandle t,
+            const std::size_t l,
+            const std::size_t m
+        ) const = 0;
+
+        virtual bool do_is_new_node(
+            moab::EntityHandle node, const std::size_t l
+        ) const = 0;
+
+        virtual double do_measure(
+            const moab::EntityHandle handle, const std::size_t l
+        ) const;
+
+        //!Interpolate the 'old' values onto the 'new' nodes, scale, and add to
+        //!'new' values.
+        //!
+        //!\param [in, out] u Nodal values of the input function.
+        //!\param [in] l Index of the MeshLevel.
+        //!\param [in] alpha Factor by which to scale the interpolant.
+        virtual moab::ErrorCode do_interpolate_old_to_new_and_axpy(
+            double * const u, std::size_t l, const double alpha
+        ) const = 0;
+
+        //!Scale a function on the 'old' nodes and add to the 'old' values.
+        //!
+        //!\param [in, out] u Nodal values of the input function.
+        //!\param [in] l Index of the MeshLevel.
+        //!\param [in] alpha Factor by which to scale the function.
+        //!\param [in] correction Function to be scaled and added.
+        virtual moab::ErrorCode do_old_values_axpy(
+            double * const u,
+            std::size_t l,
+            const double alpha,
+            double const * const correction
+        ) const = 0;
+
+        virtual
+        moab::ErrorCode do_calculate_correction_from_multilevel_component(
+            double const * const u,
+            const std::size_t l,
+            double * const correction,
+            void * const buffer
+        ) const;
+
+        virtual moab::ErrorCode do_apply_mass_matrix_to_multilevel_component(
+            double const * const u, const std::size_t l, double * const b
+        ) const = 0;
+};
+
+}
+
+#endif
---
+++ src/MeshHierarchy.cpp
@@ -0,0 +1,333 @@
+#include "MeshHierarchy.hpp"
+
+#include <algorithm>
+#include <type_traits>
+
+#include "pcg.hpp"
+#include "MassMatrix.hpp"
+
+namespace mgard {
+
+//Public member functions.
+
+MeshHierarchy::MeshHierarchy(const std::vector<MeshLevel> &meshes):
+    meshes(meshes),
+    L(meshes.size() - 1)
+{
+    //Note that `L` will wrap around if `meshes` is empty.
+    //TODO: how to deal with empty `meshes`? Currently something like
+    //    MeshHierarchy hierarchy({});
+    //    hierarchy.ndof();
+    //will cause an error.
+}
+
+std::size_t MeshHierarchy::ndof() const {
+    return ndof(L);
+}
+
+std::size_t MeshHierarchy::scratch_space_needed() const {
+    return do_scratch_space_needed();
+}
+
+moab::ErrorCode MeshHierarchy::decompose(double * const u, void *buffer) {
+    if (buffer == NULL) {
+        const std::size_t N = scratch_space_needed_for_decomposition();
+        if (scratch_space.size() < N) {
+            scratch_space.resize(N);
+        }
+        buffer = scratch_space.data();
+    }
+    return decompose(u, L, buffer);
+}
+
+moab::ErrorCode MeshHierarchy::recompose(double * const u, void *buffer) {
+    if (buffer == NULL) {
+        const std::size_t N = scratch_space_needed_for_recomposition();
+        if (scratch_space.size() < N) {
+            scratch_space.resize(N);
+        }
+        buffer = scratch_space.data();
+    }
+    //Bit of awkwardness because the recomposition involves level `l - 1` and
+    //nothing is set up to consider -1 a valid mesh index (no least because
+    //`l` is unsigned). So, though nothing is done on level 0, we can't handle
+    //that elegantly (run `recompose(u, 0, buffer)` to no effect).
+    return L ? recompose(u, 1, buffer) : moab::MB_SUCCESS;
+}
+
+//Protected member functions.
+
+MeshHierarchy::MeshHierarchy(
+    const MeshLevel &mesh,
+    const MeshRefiner refiner,
+    const std::size_t L
+):
+    meshes({mesh})
+{
+    //Reserving rather than resizing so we don't need to provide a default
+    //constructor for `MeshLevel`.
+    meshes.reserve(L + 1);
+    for (std::size_t i = 0; i + 1 != L; ++i) {
+        meshes.push_back(refiner(meshes.at(i)));
+    }
+}
+
+std::size_t MeshHierarchy::ndof(const std::size_t l) const {
+    check_mesh_index_bounds(l);
+    return do_ndof(l);
+}
+
+std::size_t MeshHierarchy::scratch_space_needed_for_decomposition() const {
+    return do_scratch_space_needed_for_decomposition();
+}
+
+std::size_t MeshHierarchy::scratch_space_needed_for_recomposition() const {
+    return do_scratch_space_needed_for_recomposition();
+}
+
+//Let `N` be the set of nodes in level `l`. `N` is the disjoint union of
+//`N_old`, the set of nodes in level `l - 1`, and `N_new`, the set of nodes
+//'new' to level `l`.
+
+moab::ErrorCode MeshHierarchy::decompose(
+    double * const u, const std::size_t l, void * const buffer
+) const {
+    check_mesh_index_bounds(l);
+    if (!l) {
+        return moab::MB_SUCCESS;
+    }
+
+    moab::ErrorCode ecode;
+    const std::size_t n = ndof(l - 1);
+    char *_buffer = static_cast<char *>(buffer);
+    //Projection of the multilevel component on level `l` onto level `l - 1`
+    //(size `n`).
+    double * const correction = reinterpret_cast<double *>(_buffer);
+    _buffer += 1 * n * sizeof(*correction);
+
+    //At this stage, the values on `N` encode `Q_{l}u`.
+
+    //Subtract the interpolant onto level `l` from the values on `N_new`.
+    ecode = subtract_interpolant_from_coarser_level_from_new_values(u, l);
+    MB_CHK_ERR(ecode);
+    //This leaves `(I - Π_{l - 1})Q_{l}u` encoded on `N_new` and
+    //`Π_{l - 1}Q_{l}u` encoded on `N_old`.
+
+    //Compute the correction `Q_{l - 1}u - Π_{l - 1}Q_{l}u` from the multilevel
+    //component `(I - Π_{l - 1})Q_{l}u`.
+    ecode = calculate_correction_from_multilevel_component(
+        u, l, correction, static_cast<void *>(_buffer)
+    );
+    MB_CHK_ERR(ecode);
+
+    //Add the correction to `Π_{l - 1}Q_{l}u`, stored on `N_old`, to obtain
+    //`Q_{l - 1}u` on `N_old`.
+    ecode = add_correction_to_old_values(u, l, correction);
+    MB_CHK_ERR(ecode);
+
+    //Continue recursively.
+    return decompose(u, l - 1, buffer);
+}
+
+moab::ErrorCode MeshHierarchy::recompose(
+    double * const u, const std::size_t l, void * const buffer
+) const {
+    check_mesh_index_bounds(l);
+    check_mesh_index_nonzero(l);
+
+    moab::ErrorCode ecode;
+    const std::size_t n = ndof(l - 1);
+    char *_buffer = static_cast<char *>(buffer);
+    double * const correction = reinterpret_cast<double *>(_buffer);
+    _buffer += 1 * n * sizeof(*correction);
+
+    //We start with `Q_{l - 1}u` on `N_old` and the multilevel component
+    //`(I - Π_{l - 1})Q_{l}u` on `N_new`. The first step is to transform
+    //`Q_{l - 1}u` to `Π_{l - 1}Q_{l}u`. This is done by projecting
+    //`(I - Π_{l - 1})Q_{l}u` to level `l - 1` (computing the 'correction') and
+    //subtracting from `Q_{l - 1}u`.
+    ecode = calculate_correction_from_multilevel_component(
+        u, l, correction, static_cast<void *>(_buffer)
+    );
+    MB_CHK_ERR(ecode);
+    //We now subtract the correction from `Q_{l - 1}u` on `N_old`.
+    ecode = subtract_correction_from_old_values(u, l, correction);
+    MB_CHK_ERR(ecode);
+
+    //This leaves us with `(I - Π_{l - 1})Q_{l}u` on `N_new` and
+    //`Π_{l - 1}Q_{l}u` on `N_old`. Once we update the values on `N_new` by
+    //adding the interpolant we'll have `Q_{l}u` on `N`.
+    add_interpolant_from_coarser_level_to_new_values(u, l);
+
+    return (l != L) ? recompose(u, l + 1, buffer) : moab::MB_SUCCESS;
+}
+
+moab::ErrorCode MeshHierarchy::calculate_correction_from_multilevel_component(
+    double const * const u,
+    const std::size_t l,
+    double * const correction,
+    void * const buffer
+) const {
+    check_mesh_index_bounds(l);
+    check_mesh_index_nonzero(l);
+    return do_calculate_correction_from_multilevel_component(
+        u, l, correction, buffer
+    );
+}
+
+moab::ErrorCode MeshHierarchy::apply_mass_matrix_to_multilevel_component(
+    double const * const u, const std::size_t l, double * const b
+) const {
+    check_mesh_index_bounds(l);
+    check_mesh_index_nonzero(l);
+    return do_apply_mass_matrix_to_multilevel_component(u, l, b);
+}
+
+moab::ErrorCode
+MeshHierarchy::subtract_interpolant_from_coarser_level_from_new_values(
+    double * const u, const std::size_t l
+) const {
+    check_mesh_index_bounds(l);
+    check_mesh_index_nonzero(l);
+    return do_interpolate_old_to_new_and_axpy(u, l, -1);
+}
+
+moab::ErrorCode
+MeshHierarchy::add_interpolant_from_coarser_level_to_new_values(
+    double * const u, const std::size_t l
+) const {
+    check_mesh_index_bounds(l);
+    check_mesh_index_nonzero(l);
+    return do_interpolate_old_to_new_and_axpy(u, l, 1);
+}
+
+moab::ErrorCode MeshHierarchy::add_correction_to_old_values(
+    double * const u,
+    const std::size_t l,
+    double const * const correction
+) const {
+    check_mesh_index_bounds(l);
+    check_mesh_index_nonzero(l);
+    return do_old_values_axpy(u, l, 1, correction);
+}
+
+moab::ErrorCode MeshHierarchy::subtract_correction_from_old_values(
+    double * const u,
+    const std::size_t l,
+    double const * const correction
+) const {
+    check_mesh_index_bounds(l);
+    check_mesh_index_nonzero(l);
+    return do_old_values_axpy(u, l, -1, correction);
+}
+
+moab::EntityHandle MeshHierarchy::replica(
+    const moab::EntityHandle node, const std::size_t l, const std::size_t m
+) const {
+    check_mesh_index_bounds(l);
+    check_mesh_index_bounds(m);
+    check_mesh_indices_nondecreasing(l, m);
+    return do_replica(node, l, m);
+}
+
+moab::Range MeshHierarchy::get_children(
+    const moab::EntityHandle t, const std::size_t l, const std::size_t m
+) const {
+    check_mesh_index_bounds(l);
+    check_mesh_index_bounds(m);
+    check_mesh_indices_nondecreasing(l, m);
+    return do_get_children(t, l, m);
+}
+
+double MeshHierarchy::measure(
+    moab::EntityHandle handle, const std::size_t l
+) const {
+    check_mesh_index_bounds(l);
+    return do_measure(handle, l);
+}
+
+bool MeshHierarchy::is_new_node(
+    moab::EntityHandle node, const std::size_t l
+) const {
+    check_mesh_index_bounds(l);
+    return do_is_new_node(node, l);
+}
+
+void MeshHierarchy::check_mesh_index_bounds(const std::size_t l) const {
+    //`l` is guaranteed to be nonnegative because `std::size_t` is unsigned.
+    if (!(std::is_unsigned<std::size_t>::value && l <= L)) {
+        throw std::out_of_range("mesh index out of range encountered");
+    }
+}
+
+void MeshHierarchy::check_mesh_indices_nondecreasing(
+    const std::size_t l, const std::size_t m
+) const {
+    if (!(l <= m)) {
+        throw std::invalid_argument("mesh indices should be nondecreasing");
+    }
+}
+
+void MeshHierarchy::check_mesh_index_nonzero(const std::size_t l) const {
+    if (!l) {
+        throw std::out_of_range("mesh index should be nonzero");
+    }
+}
+
+//Private member functions.
+
+std::size_t MeshHierarchy::do_ndof(const std::size_t l) const {
+    return meshes.at(l).ndof();
+}
+
+std::size_t MeshHierarchy::do_scratch_space_needed() const {
+    std::vector<std::size_t> sizes = {
+        scratch_space_needed_for_decomposition(),
+        scratch_space_needed_for_recomposition()
+    };
+    return *std::max_element(sizes.begin(), sizes.end());
+}
+
+std::size_t MeshHierarchy::do_scratch_space_needed_for_decomposition() const {
+    return L ? 6 * ndof(L - 1) : 0;
+}
+
+double MeshHierarchy::do_measure(
+    const moab::EntityHandle handle, const std::size_t l
+) const {
+    return meshes.at(l).measure(handle);
+}
+
+moab::ErrorCode
+MeshHierarchy::do_calculate_correction_from_multilevel_component(
+    double const * const u,
+    const std::size_t l,
+    double * const correction,
+    void * const buffer
+) const {
+    const MeshLevel &mesh = meshes.at(l - 1);
+    //As of this writing, this is already computed in the functions calling this
+    //function. Could reuse it, or maybe split up `buffer`.
+    const std::size_t n = ndof(l - 1);
+
+    char *_buffer = static_cast<char *>(buffer);
+    //Product of the mass matrix on level `l - 1` and the multilevel component
+    //on level `l` (size `n`) (with restriction from level `l` to level `l - 1`
+    //built in).
+    double *b = reinterpret_cast<double *>(_buffer);
+    _buffer += 1 * n * sizeof(*b);
+    //Scratch space for the PCG implementation (size `4 * n`).
+    double *pcg_buffer = reinterpret_cast<double *>(_buffer);
+    _buffer += 4 * n * sizeof(*pcg_buffer);
+
+    //Apply the mass matrix to get the righthand side of our system.
+    moab::ErrorCode ecode = apply_mass_matrix_to_multilevel_component(u, l, b);
+    MB_CHK_ERR(ecode);
+    //Invert the system to obtain `Q_{l - 1}u - Π_{l - 1}Q_{l}u` on `N_old`.
+    const MassMatrix M(&mesh);
+    const MassMatrixPreconditioner P(&mesh);
+    helpers::pcg(M, b, P, correction, pcg_buffer);
+    return moab::MB_SUCCESS;
+}
+
+}
