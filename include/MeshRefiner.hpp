97f94e1b8617a969df9c939b81ef6df6d28ff7d0
Ben Whitney <whitneybe@ornl.gov>
2019-09-26 11:59:09 -0400

    Add function object for uniform mesh refinement.

--- include/MeshRefiner.hpp
+++ include/MeshRefiner.hpp
@@ -9,9 +9,19 @@ namespace mgard {
 
 class MeshRefiner {
     public:
+        //!Refine a mesh.
+        //!
+        //!\param mesh Mesh to be refined.
+        //!
+        //!\return Mesh obtained by refining the input mesh.
         MeshLevel operator()(const MeshLevel &mesh);
 
     private:
+        //!Refine a mesh.
+        //!
+        //!\param mesh Mesh to be refined.
+        //!
+        //!\return Mesh obtained by refining the input mesh.
         virtual MeshLevel do_operator_parentheses(const MeshLevel &mesh) = 0;
 };
 
