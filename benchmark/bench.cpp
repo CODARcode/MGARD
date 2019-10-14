8e4d93ee1cc121712155852a807306f88c83dc8d
NAThompson <nathompson7@protonmail.com>
2019-10-14 11:08:06 -0400

    Add benchmark of linear data.

--- benchmark/bench.cpp
+++ benchmark/bench.cpp
@@ -3,7 +3,27 @@
 
 #include "mgard_api.h"
 
-static void BM_MGARD(benchmark::State& state) {
+static void BM_MGARDLinear(benchmark::State& state) {
+
+  std::vector<double> v(state.range(0));
+  for (size_t i = 0; i < v.size(); ++i) {
+    v[i] = 3.0*i;
+  }
+
+  int out_size;
+  double tol = 1e-3;
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(mgard_compress(/* double = 0 */ 0, v.data(), out_size,  4,  v.size()/4,  1, tol));
+  }
+
+  state.SetComplexityN(state.range(0));
+  state.SetBytesProcessed(int64_t(state.range(0))*int64_t(state.iterations())*int64_t(sizeof(double)));
+}
+
+BENCHMARK(BM_MGARDLinear)->RangeMultiplier(2)->Range(1<<4, 1<<22)->Complexity();
+
+
+static void BM_MGARDRandom(benchmark::State& state) {
   std::mt19937_64 gen(124124);
   std::normal_distribution<double> dis{0,1};
 
@@ -22,6 +42,6 @@ static void BM_MGARD(benchmark::State& state) {
   state.SetBytesProcessed(int64_t(state.range(0))*int64_t(state.iterations())*int64_t(sizeof(double)));
 }
 
-BENCHMARK(BM_MGARD)->RangeMultiplier(2)->Range(1<<4, 1<<22)->Complexity();
+BENCHMARK(BM_MGARDRandom)->RangeMultiplier(2)->Range(1<<4, 1<<22)->Complexity();
 
 BENCHMARK_MAIN();
