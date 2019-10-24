a80a5401906493fbde6ab3c3c1a907410d91e71e
Ben Whitney <whitneybe@ornl.gov>
2019-10-24 17:22:45 -0400

    Merge structured and unstructured makefiles.

--- tests/src/testing_utilities.cpp
+++ tests/src/testing_utilities.cpp
@@ -6,12 +6,12 @@
 
 #include "moab/EntityType.hpp"
 
-std::filesystem::path mesh_path(const std::string &filename) {
-    return std::filesystem::path("tests") / "meshes" / filename;
+std::experimental::filesystem::path mesh_path(const std::string &filename) {
+    return std::experimental::filesystem::path("tests") / "meshes" / filename;
 }
 
-std::filesystem::path output_path(const std::string &filename) {
-    return std::filesystem::path("tests") / "outputs" / filename;
+std::experimental::filesystem::path output_path(const std::string &filename) {
+    return std::experimental::filesystem::path("tests") / "outputs" / filename;
 }
 
 void require_moab_success(const moab::ErrorCode ecode) {
