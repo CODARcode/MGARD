30a2c7a60733a6f9819b3b1b63c2a88b6eda61ad
Gary Liu <qing.liu@njit.edu>
2019-10-30 09:09:05 -0400

    tests work-in-progress.

--- scripts/travis/run-gs.sh
+++ scripts/travis/run-gs.sh
@@ -11,18 +11,6 @@ then
   exit 1
 fi
 
-wget https://github.com/ornladios/ADIOS2/archive/v2.5.0.tar.gz
-tar -zxvf v2.5.0.tar.gz
-
 cd ${SOURCE_DIR}
-
-
-./scripts/
-
-# Check python code with flake8
-if ! ~/.local/bin/flake8 --config=flake8.cfg .
-then
-  exit 3
-fi
 
 exit 0
