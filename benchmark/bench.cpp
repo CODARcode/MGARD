3ed96c251465a244f69691c83dd3b2835bb4cde4
Ben Whitney <whitneybe@ornl.gov>
2019-11-01 13:09:46 -0400

    Add unstructured {de,re}composition benchmarks.

--- benchmark/bench.cpp
+++ benchmark/bench.cpp
@@ -1,7 +1,14 @@
-#include <random>
 #include <benchmark/benchmark.h>
 
+#include <random>
+#include <string>
+
+#include "moab/Core.hpp"
+
 #include "mgard_api.h"
+
+#include "MeshLevel.hpp"
+#include "UniformMeshHierarchy.hpp"
 
 static void BM_MGARD(benchmark::State& state) {
   std::mt19937_64 gen(124124);
@@ -21,7 +28,83 @@ static void BM_MGARD(benchmark::State& state) {
   state.SetComplexityN(state.range(0));
   state.SetBytesProcessed(int64_t(state.range(0))*int64_t(state.iterations())*int64_t(sizeof(double)));
 }
-
 BENCHMARK(BM_MGARD)->RangeMultiplier(2)->Range(1<<4, 1<<22)->Complexity();
+
+static mgard::UniformMeshHierarchy read_mesh_and_refine(
+    moab::Core &mbcore, const std::string &filename, const std::size_t L
+) {
+    const std::string filepath = "tests/meshes/" + filename;
+    moab::ErrorCode ecode = mbcore.load_file(filepath.c_str());
+    assert(ecode == moab::MB_SUCCESS);
+    return mgard::UniformMeshHierarchy(mgard::MeshLevel(mbcore), L);
+}
+
+static void BM_unstructured_decompose(
+    benchmark::State &state, const std::string &filename
+) {
+    std::default_random_engine gen;
+    std::uniform_real_distribution<double> dis(-1, 1);
+
+    const int64_t L = state.range(0);
+    moab::Core mbcore;
+    mgard::UniformMeshHierarchy hierarchy = read_mesh_and_refine(
+        mbcore, filename, L
+    );
+
+    const std::size_t N = hierarchy.ndof();
+    std::vector<double> u(N);
+    for (double &x : u) {
+        x = dis(gen);
+    }
+
+    //Could preallocate buffer needed for decomposition.
+    for (auto _ : state) {
+        moab::ErrorCode ecode = hierarchy.decompose(u.data());
+        assert(ecode == moab::MB_SUCCESS);
+    }
+
+    //Could alternatively count up all the entities in all the levels.
+    state.SetComplexityN(N);
+    state.SetBytesProcessed(state.iterations() * N * sizeof(double));
+}
+BENCHMARK_CAPTURE(
+    BM_unstructured_decompose, circle, "circle.msh"
+)->DenseRange(1, 6, 1)->Complexity()->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(
+    BM_unstructured_decompose, seated, "seated.msh"
+)->DenseRange(3, 9, 1)->Complexity()->Unit(benchmark::kMillisecond);
+
+static void BM_unstructured_recompose(
+    benchmark::State &state, const std::string &filename
+) {
+    std::default_random_engine gen;
+    std::uniform_real_distribution<double> dis(-2, 0);
+
+    const int64_t L = state.range(0);
+    moab::Core mbcore;
+    mgard::UniformMeshHierarchy hierarchy = read_mesh_and_refine(
+        mbcore, filename, L
+    );
+
+    const std::size_t N = hierarchy.ndof();
+    std::vector<double> u(N);
+    for (double &x : u) {
+        x = dis(gen);
+    }
+
+    //Could preallocate buffer needed for recomposition.
+    for (auto _ : state) {
+        moab::ErrorCode ecode = hierarchy.recompose(u.data());
+        assert(ecode == moab::MB_SUCCESS);
+    }
+
+    //Could alternatively count up all the entities in all the levels.
+    state.SetComplexityN(N);
+    state.SetBytesProcessed(state.iterations() * N * sizeof(double));
+}
+BENCHMARK_CAPTURE(BM_unstructured_recompose, circle, "circle.msh")
+    ->DenseRange(1, 6, 1)->Complexity()->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(BM_unstructured_recompose, pyramid, "pyramid.msh")
+    ->DenseRange(3, 8, 1)->Complexity()->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
