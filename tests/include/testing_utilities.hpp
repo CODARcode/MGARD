155f96a775b46483b657f3a1f440e14bd1b66055
Ben Whitney <whitneybe@ornl.gov>
2019-11-11 19:41:35 -0500

    Add approximate vector equality test function.

--- tests/include/testing_utilities.hpp
+++ tests/include/testing_utilities.hpp
@@ -1,3 +1,6 @@
+#ifndef TESTING_UTILITIES_HPP
+#define TESTING_UTILITIES_HPP
+
 #include <cstddef>
 
 #include <experimental/filesystem>
@@ -5,10 +8,25 @@
 
 #include "moab/Interface.hpp"
 
-#include "MeshLevel.hpp"
+static const double APPROX_MARGIN_DEFAULT = 0;
 
 std::experimental::filesystem::path mesh_path(const std::string &filename);
 
 std::experimental::filesystem::path output_path(const std::string &filename);
 
 void require_moab_success(const moab::ErrorCode ecode);
+
+//`T` and `U` should be iterators dereferencing to `double` or similar.
+template <typename T, typename U, typename SizeType>
+void require_vector_equality(
+    T p, U q, const SizeType N, const double margin = APPROX_MARGIN_DEFAULT
+);
+
+//`T` and `U` should be `SequenceContainer`s to `double` or similar.
+template <typename T, typename U>
+void require_vector_equality(
+    const T &t, const U &u, const double margin = APPROX_MARGIN_DEFAULT
+);
+
+#include "testing_utilities.tpp"
+#endif
