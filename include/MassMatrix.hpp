6ac9bbcb2aaf34a9e653815d6ee2ef0da724b558
Ben Whitney <whitneybe@ornl.gov>
2019-11-19 19:02:53 -0500

    Combine mass matrix implementations.

--- include/MassMatrix.hpp
+++ include/MassMatrix.hpp
@@ -13,55 +13,23 @@
 namespace mgard {
 
 //!Mass matrix for continuous piecewise linear functions defined on a
-//!`MeshLevel`.
-class MassMatrix: public LinearOperator {
+//!`MeshLevel` where a subset of the hat functions are allowed to be nonzero.
+class SubsetMassMatrix: public LinearOperator {
     public:
         //!Constructor.
         //!
         //!\param mesh Underlying mesh.
-        MassMatrix(const MeshLevel &mesh);
+        //!\param nodes Subset nodes whose associated hat functions are allowed
+        //!to be nonzero. This parameter determines the ordering of the input
+        //!and output vectors.
+        SubsetMassMatrix(const MeshLevel &mesh, const moab::Range &nodes);
 
-    private:
-        //!Underlying mesh.
-        const MeshLevel &mesh;
-
-        virtual void do_operator_parentheses(
-            double const * const x, double * const b
-        ) const override;
-};
-
-//!Mass matrix for continuous piecewise linear functions defined on a
-//!`MeshLevel` where only a subset (contiguous by node ordering) of the hat
-//!functions are allowed to be nonzero.
-//!
-//!This is used to compute the \f$ L^{2} \f$ norm of functions in the image of
-//!\f$ I -\Pi_{\ell} \f$ (with \f$ I \f$ the identity and \f$ \Pi_{\ell} \f$ the
-//!continuous piecewise linear interpolation onto the next coarser mesh in the
-//!hierarchy). Currently, 'new' nodes are contiguous.
-class ContiguousSubsetMassMatrix: public LinearOperator {
-    public:
-        //!Constructor.
-        //!
-        //!\param mesh Underlying mesh.
-        //!\param nodes Contiguous range of nodes whose associated hat functions
-        //!are allowed to be nonzero. This parameter determines the ordering of
-        //!the input and output vectors.
-        ContiguousSubsetMassMatrix(
-            const MeshLevel &mesh, const moab::Range nodes
-        );
-
-    private:
+    protected:
         //!Underlying mesh.
         const MeshLevel &mesh;
 
         //!'Allowed' node range.
-        const moab::Range nodes;
-
-        //!First (by handle) 'allowed' node.
-        const moab::EntityHandle min_node;
-
-        //!Last (by handle) 'allowed' node.
-        const moab::EntityHandle max_node;
+        const moab::Range &nodes;
 
         //!Find the index (as determined by the subset) of a node. Does not
         //!perform any error checking.
@@ -76,9 +44,65 @@ class ContiguousSubsetMassMatrix: public LinearOperator {
         //!\param node Handle of the node.
         bool contains(const moab::EntityHandle node) const;
 
+    private:
         virtual void do_operator_parentheses(
             double const * const x, double * const b
         ) const override;
+
+        virtual std::size_t do_index(const moab::EntityHandle node) const = 0;
+
+        virtual bool do_contains(const moab::EntityHandle node) const = 0;
+};
+
+//!Mass matrix for continuous piecewise linear functions defined on a
+//!`MeshLevel`.
+class MassMatrix: public SubsetMassMatrix {
+    public:
+        //!Constructor.
+        //!
+        //!\param mesh Underlying mesh.
+        MassMatrix(const MeshLevel &mesh);
+
+    private:
+        virtual std::size_t do_index(
+            const moab::EntityHandle node
+        ) const override;
+
+        virtual bool do_contains(const moab::EntityHandle node) const override;
+};
+
+//!Mass matrix for continuous piecewise linear functions defined on a
+//!`MeshLevel` where a subset (contiguous by node ordering) of the hat
+//!functions are allowed to be nonzero.
+//!
+//!This is used to compute the \f$ L^{2} \f$ norm of functions in the image of
+//!\f$ I -\Pi_{\ell} \f$ (with \f$ I \f$ the identity and \f$ \Pi_{\ell} \f$ the
+//!continuous piecewise linear interpolation onto the next coarser mesh in the
+//!hierarchy). Currently, 'new' nodes are contiguous.
+class ContiguousSubsetMassMatrix: public SubsetMassMatrix {
+    public:
+        //!Constructor.
+        //!
+        //!\param mesh Underlying mesh.
+        //!\param nodes Contiguous range of nodes whose associated hat functions
+        //!are allowed to be nonzero. This parameter determines the ordering of
+        //!the input and output vectors.
+        ContiguousSubsetMassMatrix(
+            const MeshLevel &mesh, const moab::Range &nodes
+        );
+
+    private:
+        //!First (by handle) 'allowed' node.
+        const moab::EntityHandle min_node;
+
+        //!Last (by handle) 'allowed' node.
+        const moab::EntityHandle max_node;
+
+        virtual std::size_t do_index(
+            const moab::EntityHandle node
+        ) const override;
+
+        virtual bool do_contains(const moab::EntityHandle node) const override;
 };
 
 //!Preconditioner for `MassMatrix`.
--- src/MassMatrix.cpp
+++ src/MassMatrix.cpp
@@ -9,30 +9,35 @@ namespace mgard {
 
 //Public member functions.
 
-MassMatrix::MassMatrix(const MeshLevel &mesh):
-    LinearOperator(mesh.ndof()),
-    mesh(mesh)
-{
-    mesh.precompute_element_measures();
-}
-
-ContiguousSubsetMassMatrix::ContiguousSubsetMassMatrix(
-    const MeshLevel &mesh, const moab::Range nodes
+SubsetMassMatrix::SubsetMassMatrix(
+    const MeshLevel &mesh, const moab::Range &nodes
 ):
     LinearOperator(nodes.size()),
     mesh(mesh),
-    nodes(nodes),
+    nodes(nodes)
+{
+    if (!mesh.entities[moab::MBVERTEX].contains(nodes)) {
+        throw std::invalid_argument(
+            "Node set must be a subset of the nodes of the mesh."
+        );
+    }
+    mesh.precompute_element_measures();
+}
+
+MassMatrix::MassMatrix(const MeshLevel &mesh):
+    SubsetMassMatrix(mesh, mesh.entities[moab::MBVERTEX])
+{
+}
+
+ContiguousSubsetMassMatrix::ContiguousSubsetMassMatrix(
+    const MeshLevel &mesh, const moab::Range &nodes
+):
+    SubsetMassMatrix(mesh, nodes),
     min_node(nodes.front()),
     max_node(nodes.back())
 {
     if (nodes.psize() != 1) {
-        throw std::invalid_argument("Node subset must be contiguous.");
-    }
-    moab::Range intersection;
-    if (moab::intersect(mesh.entities[moab::MBVERTEX], nodes) != nodes) {
-        throw std::invalid_argument(
-            "Node subset must be contained within the nodes of the mesh."
-        );
+        throw std::invalid_argument("Node set must be contiguous.");
     }
 }
 
@@ -43,49 +48,19 @@ MassMatrixPreconditioner::MassMatrixPreconditioner(const MeshLevel &mesh):
     mesh.precompute_element_measures();
 }
 
+//Protected member functions.
+
+std::size_t SubsetMassMatrix::index(const moab::EntityHandle node) const {
+    return do_index(node);
+}
+
+bool SubsetMassMatrix::contains(const moab::EntityHandle node) const {
+    return do_contains(node);
+}
+
 //Private member functions.
 
-void MassMatrix::do_operator_parentheses(
-    double const * const x, double * const b
-) const {
-    std::fill(b, b + range_dimension, 0);
-    const moab::Range &elements = mesh.entities[mesh.element_type];
-    const std::size_t measure_factor_divisor = (
-        (mesh.topological_dimension + 1) * (mesh.topological_dimension + 2)
-    );
-    for (moab::EntityHandle element : elements) {
-        const double measure_factor = (
-            mesh.measure(element) / measure_factor_divisor
-        );
-        //Pairs `(i, v)` where `i` is the global index of a node and `v` is
-        //`x[i]`, the value of the function there.
-        std::vector<std::pair<std::size_t, double>> nodal_pairs;
-        nodal_pairs.reserve(mesh.num_nodes_per_element);
-        double nodal_values_sum = 0;
-        for (const moab::EntityHandle node : mesh.connectivity(element)) {
-            std::pair<std::size_t, double> pair;
-            nodal_values_sum += pair.second = x[pair.first = mesh.index(node)];
-            nodal_pairs.push_back(pair);
-        }
-        for (std::pair<std::size_t, double> pair : nodal_pairs) {
-            b[pair.first] += measure_factor * (nodal_values_sum + pair.second);
-        }
-    }
-}
-
-std::size_t ContiguousSubsetMassMatrix::index(
-    const moab::EntityHandle node
-) const {
-    return node - min_node;
-}
-
-bool ContiguousSubsetMassMatrix::contains(const moab::EntityHandle node) const {
-    return min_node <= node && node <= max_node;
-}
-
-//This is just a slight modification of analogous `MassMatrix` function. Could
-//somehow merge them.
-void ContiguousSubsetMassMatrix::do_operator_parentheses(
+void SubsetMassMatrix::do_operator_parentheses(
     double const * const x, double * const b
 ) const {
     std::fill(b, b + range_dimension, 0);
@@ -99,7 +74,8 @@ void ContiguousSubsetMassMatrix::do_operator_parentheses(
         );
         //Pairs `(i, v)` where `i` is the global index of a node and `v` is
         //`x[i]`, the value of the function there. Here 'global' refers to the
-        //index in `nodes`/`x`/`b`, not the mesh.
+        //index in `nodes`/`x`/`b`, not the necessarily mesh. (They're the same
+        //in `MassMatrix`.)
         std::vector<std::pair<std::size_t, double>> nodal_pairs;
         nodal_pairs.reserve(mesh.num_nodes_per_element);
         double nodal_values_sum = 0;
@@ -115,6 +91,26 @@ void ContiguousSubsetMassMatrix::do_operator_parentheses(
             b[pair.first] += measure_factor * (nodal_values_sum + pair.second);
         }
     }
+}
+
+std::size_t MassMatrix::do_index(const moab::EntityHandle node) const {
+    return mesh.index(node);
+}
+
+bool MassMatrix::do_contains(const moab::EntityHandle) const {
+    return true;
+}
+
+std::size_t ContiguousSubsetMassMatrix::do_index(
+    const moab::EntityHandle node
+) const {
+    return node - min_node;
+}
+
+bool ContiguousSubsetMassMatrix::do_contains(
+    const moab::EntityHandle node
+) const {
+    return min_node <= node && node <= max_node;
 }
 
 void MassMatrixPreconditioner::do_operator_parentheses(
