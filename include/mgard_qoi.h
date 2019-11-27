12fcb2425fca4d05660a4c7badc3085e27405a15
Ben Whitney <whitneybe@ornl.gov>
2019-11-27 11:46:12 -0500

    Template quantity of interest/`mgard_qoi` header.

--- include/mgard_qoi.h
+++ include/mgard_qoi.h
@@ -5,17 +5,16 @@
 //
 // version: 0.0.0.2
 // See LICENSE for details.
-
 #ifndef MGARD_QOI_H
 #define MGARD_QOI_H
 
+#include <vector>
+
 namespace mgard_qoi {
 
-double qoi_ave(const int nrow, const int ncol, const int nfib,
-               std::vector<double> u);
-
-float qoi_ave(const int nrow, const int ncol, const int nfib,
-              std::vector<float> u);
+template <typename Real>
+Real qoi_ave(const int nrow, const int ncol, const int nfib,
+             std::vector<Real> u);
 
 } // namespace mgard_qoi
 
