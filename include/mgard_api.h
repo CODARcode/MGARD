e4312670041aba3000f8c499d45256701f577aee
Ben Whitney <whitneybe@ornl.gov>
2019-11-27 11:04:13 -0500

    Template API/`mgard_api` headers.

--- include/mgard_api.h
+++ include/mgard_api.h
@@ -4,114 +4,11 @@
 // Corresponding Author: Ozan Tugluk
 //
 // version: 0.0.0.2
-// See LICENSE for details
-
-// DOUBLE PRECISON version of MGARD API //
-
-#include "mgard.h"
-
+// See LICENSE for details.
 #ifndef MGARD_API_H
 #define MGARD_API_H
+// Double-precision/`double` version of API.
 
-/// Use this version of mgard_compress to compress your data with a tolerance
-/// measured in  relative L-infty norm, version 1 for equispaced grids, 1a for
-/// tensor product grids with arbitrary spacing
-
-unsigned char *mgard_compress(int itype_flag, double *data, int &out_size,
-                              int n1, int n2, int n3, double tol); // ...  1
-unsigned char *mgard_compress(int itype_flag, double *data, int &out_size,
-                              int n1, int n2, int n3,
-                              std::vector<double> &coords_x,
-                              std::vector<double> &coords_y,
-                              std::vector<double> &coords_z,
-                              double tol); // ... 1a
-
-// Use this version of mgard_compress to compress your data with a tolerance
-// measured in  relative s-norm.
-// Set s=0 for L2-norm
-// 2)
-unsigned char *mgard_compress(int itype_flag, double *data, int &out_size,
-                              int n1, int n2, int n3, double tol,
-                              double s); // ... 2
-unsigned char *mgard_compress(int itype_flag, double *data, int &out_size,
-                              int n1, int n2, int n3,
-                              std::vector<double> &coords_x,
-                              std::vector<double> &coords_y,
-                              std::vector<double> &coords_z, double tol,
-                              double s); // ... 2a
-
-// Use this version of mgard_compress to compress your data to preserve the
-// error in a given quantity of interest Here qoi denotes the quantity of
-// interest  which is a bounded linear functional in s-norm. This version
-// recomputes the s-norm of the supplied linear functional every time it is
-// invoked. If the same functional is to be reused for different sets of data
-// then you are recommended to use one of the functions below (4, 5) to compute
-// and store the norm and call MGARD using (6).
-//
-
-unsigned char *mgard_compress(int itype_flag, double *data, int &out_size,
-                              int n1, int n2, int n3, double tol,
-                              double (*qoi)(int, int, int, double *),
-                              double s); // ... 3
-unsigned char *mgard_compress(int itype_flag, double *data, int &out_size,
-                              int n1, int n2, int n3,
-                              std::vector<double> &coords_x,
-                              std::vector<double> &coords_y,
-                              std::vector<double> &coords_z, double tol,
-                              double (*qoi)(int, int, int, double *),
-                              double s); // ... 3a
-
-// Use this version of mgard_compress to compute the  s-norm of a quantity of
-// interest. Store this for further use if you wish to work with the same qoi in
-// the future for different datasets.
-double mgard_compress(int n1, int n2, int n3,
-                      double (*qoi)(int, int, int, std::vector<double>),
-                      double s); // ... 4
-double mgard_compress(int n1, int n2, int n3, std::vector<double> &coords_x,
-                      std::vector<double> &coords_y,
-                      std::vector<double> &coords_z,
-                      double (*qoi)(int, int, int, std::vector<double>),
-                      double s); // ... 4a
-
-// c-compatible version
-double mgard_compress(int n1, int n2, int n3,
-                      double (*qoi)(int, int, int, double *),
-                      double s); // ... 5
-double mgard_compress(int n1, int n2, int n3, std::vector<double> &coords_x,
-                      std::vector<double> &coords_y,
-                      std::vector<double> &coords_z,
-                      double (*qoi)(int, int, int, double *),
-                      double s); // ... 5a
-
-// Use this version of mgard_compress to compress your data with a tolerance in
-// -s norm with given s-norm of quantity of interest qoi
-unsigned char *mgard_compress(int itype_flag, double *data, int &out_size,
-                              int n1, int n2, int n3, double tol,
-                              double norm_of_qoi, double s); // ... 6
-unsigned char *mgard_compress(int itype_flag, double *data, int &out_size,
-                              int n1, int n2, int n3,
-                              std::vector<double> &coords_x,
-                              std::vector<double> &coords_y,
-                              std::vector<double> &coords_z, double tol,
-                              double norm_of_qoi, double s); // ... 6a
-
-double *mgard_decompress(int itype_flag, double &quantizer, unsigned char *data,
-                         int data_len, int n1, int n2,
-                         int n3); // decompress L-infty compressed data
-double *mgard_decompress(
-    int itype_flag, double &quantizer, unsigned char *data, int data_len,
-    int n1, int n2, int n3, std::vector<double> &coords_x,
-    std::vector<double> &coords_y,
-    std::vector<double> &coords_z); // decompress L-infty compressed data
-
-double *mgard_decompress(int itype_flag, double &quantizer, unsigned char *data,
-                         int data_len, int n1, int n2, int n3,
-                         double s); // decompress s-norm
-double *mgard_decompress(int itype_flag, double &quantizer, unsigned char *data,
-                         int data_len, int n1, int n2, int n3,
-                         std::vector<double> &coords_x,
-                         std::vector<double> &coords_y,
-                         std::vector<double> &coords_z,
-                         double s); // decompress s-norm
+#include "mgard_api_templates.hpp"
 
 #endif
---
+++ include/mgard_api_templates.hpp
@@ -0,0 +1,126 @@
+#include <vector>
+
+/// Use this version of mgard_compress to compress your data with a tolerance
+/// measured in  relative L-infty norm, version 1 for equispaced grids, 1a for
+/// tensor product grids with arbitrary spacing
+
+template <typename Real>
+unsigned char *mgard_compress(int itype_flag, Real *data, int &out_size,
+                              int n1, int n2, int n3, Real tol); // ...  1
+
+template <typename Real>
+unsigned char *mgard_compress(int itype_flag, Real *data, int &out_size,
+                              int n1, int n2, int n3,
+                              std::vector<Real> &coords_x,
+                              std::vector<Real> &coords_y,
+                              std::vector<Real> &coords_z,
+                              Real tol); // ... 1a
+
+// Use this version of mgard_compress to compress your data with a tolerance
+// measured in  relative s-norm.
+// Set s=0 for L2-norm
+// 2)
+template <typename Real>
+unsigned char *mgard_compress(int itype_flag, Real *data, int &out_size,
+                              int n1, int n2, int n3, Real tol,
+                              Real s); // ... 2
+
+template <typename Real>
+unsigned char *mgard_compress(int itype_flag, Real *data, int &out_size,
+                              int n1, int n2, int n3,
+                              std::vector<Real> &coords_x,
+                              std::vector<Real> &coords_y,
+                              std::vector<Real> &coords_z, Real tol,
+                              Real s); // ... 2a
+
+// Use this version of mgard_compress to compress your data to preserve the
+// error in a given quantity of interest Here qoi denotes the quantity of
+// interest  which is a bounded linear functional in s-norm. This version
+// recomputes the s-norm of the supplied linear functional every time it is
+// invoked. If the same functional is to be reused for different sets of data
+// then you are recommended to use one of the functions below (4, 5) to compute
+// and store the norm and call MGARD using (6).
+//
+
+template <typename Real>
+unsigned char *mgard_compress(int itype_flag, Real *data, int &out_size,
+                              int n1, int n2, int n3, Real tol,
+                              Real (*qoi)(int, int, int, Real *),
+                              Real s); // ... 3
+
+template <typename Real>
+unsigned char *mgard_compress(int itype_flag, Real *data, int &out_size,
+                              int n1, int n2, int n3,
+                              std::vector<Real> &coords_x,
+                              std::vector<Real> &coords_y,
+                              std::vector<Real> &coords_z, Real tol,
+                              Real (*qoi)(int, int, int, Real *),
+                              Real s); // ... 3a
+
+// Use this version of mgard_compress to compute the  s-norm of a quantity of
+// interest. Store this for further use if you wish to work with the same qoi in
+// the future for different datasets.
+template <typename Real>
+Real mgard_compress(int n1, int n2, int n3,
+                    Real (*qoi)(int, int, int, std::vector<Real>),
+                    Real s); // ... 4
+
+template <typename Real>
+Real mgard_compress(int n1, int n2, int n3, std::vector<Real> &coords_x,
+                    std::vector<Real> &coords_y,
+                    std::vector<Real> &coords_z,
+                    Real (*qoi)(int, int, int, std::vector<Real>),
+                    Real s); // ... 4a
+
+// c-compatible version
+template <typename Real>
+Real mgard_compress(int n1, int n2, int n3,
+                    Real (*qoi)(int, int, int, Real *),
+                    Real s); // ... 5
+
+template <typename Real>
+Real mgard_compress(int n1, int n2, int n3, std::vector<Real> &coords_x,
+                    std::vector<Real> &coords_y,
+                    std::vector<Real> &coords_z,
+                    Real (*qoi)(int, int, int, Real *),
+                    Real s); // ... 5a
+
+// Use this version of mgard_compress to compress your data with a tolerance in
+// -s norm with given s-norm of quantity of interest qoi
+template <typename Real>
+unsigned char *mgard_compress(int itype_flag, Real *data, int &out_size,
+                              int n1, int n2, int n3, Real tol,
+                              Real norm_of_qoi, Real s); // ... 6
+
+template <typename Real>
+unsigned char *mgard_compress(int itype_flag, Real *data, int &out_size,
+                              int n1, int n2, int n3,
+                              std::vector<Real> &coords_x,
+                              std::vector<Real> &coords_y,
+                              std::vector<Real> &coords_z, Real tol,
+                              Real norm_of_qoi, Real s); // ... 6a
+
+template <typename Real>
+Real *mgard_decompress(int itype_flag, Real &quantizer, unsigned char *data,
+                         int data_len, int n1, int n2,
+                         int n3); // decompress L-infty compressed data
+
+template <typename Real>
+Real *mgard_decompress(
+    int itype_flag, Real &quantizer, unsigned char *data, int data_len,
+    int n1, int n2, int n3, std::vector<Real> &coords_x,
+    std::vector<Real> &coords_y,
+    std::vector<Real> &coords_z); // decompress L-infty compressed data
+
+template <typename Real>
+Real *mgard_decompress(int itype_flag, Real &quantizer, unsigned char *data,
+                       int data_len, int n1, int n2, int n3,
+                       Real s); // decompress s-norm
+
+template <typename Real>
+Real *mgard_decompress(int itype_flag, Real &quantizer, unsigned char *data,
+                       int data_len, int n1, int n2, int n3,
+                       std::vector<Real> &coords_x,
+                       std::vector<Real> &coords_y,
+                       std::vector<Real> &coords_z,
+                       Real s); // decompress s-norm
