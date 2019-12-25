866a6b8d81c1793dc22da7b0f73a0dc28915cadc
Gary Liu <qing.liu@njit.edu>
2019-12-24 23:51:42 -0500

    format compliance.

--- include/mgard.h
+++ include/mgard.h
@@ -117,12 +117,10 @@ refactor_qz(int nrow, int ncol, int nfib, std::vector<Real> &coords_x,
             const Real *v, int &outsize, Real tol, Real s);
 
 template <typename Real>
-unsigned char *refactor_qz_1D(int n, const Real *v, int &outsize,
-                              Real tol);
+unsigned char *refactor_qz_1D(int n, const Real *v, int &outsize, Real tol);
 
 template <typename Real>
-unsigned char *refactor_qz_1D(int n, const Real *v, int &outsize,
-                              Real tol, Real s);
+unsigned char *refactor_qz_1D(int n, const Real *v, int &outsize, Real tol, Real s);
 
 template <typename Real>
 unsigned char *refactor_qz_2D(int nrow, int ncol, const Real *v, int &outsize,
