8f0b07879f7aa648132995852509c560ae65e622
Ben Whitney <whitneybe@ornl.gov>
2020-01-02 14:38:18 -0500

    Add linear quantizer.

---
+++ include/Quantizer.hpp
@@ -0,0 +1,51 @@
+#ifndef QUANTIZER_HPP
+#define QUANTIZER_HPP
+//!\file
+//!\brief Quantizers to round floating point numbers in preparation for lossless
+//!compression.
+
+#include <type_traits>
+
+namespace mgard {
+
+template <typename Real, typename Int>
+class Quantizer {
+
+    static_assert(std::is_floating_point<Real>::value);
+    static_assert(std::is_integral<Int>::value);
+    static_assert(std::is_signed<Int>::value);
+
+    public:
+        //!Constructor.
+        //!
+        //!\param quantum Spacing between adjacent quantized numbers.
+        Quantizer(const Real quantum);
+
+        //!Quantize a floating point number to an integer.
+        //!
+        //!\param x Number to be quantized.
+        Int quantize(const Real x) const;
+
+        //!Dequantize an integer to a floating point number.
+        //!
+        //!\param n Number to be dequantized.
+        Real dequantize(const Int n) const;
+
+    private:
+        //!Spacing between adjacent quantized numbers.
+        const Real quantum;
+
+        //!Greatest value which cannot be quantized using `quantum`.
+        const Real minimum;
+
+        //!Least value which cannot be quantized using `quantum`.
+        const Real maximum;
+
+        virtual Int do_quantize(const Real x) const;
+        virtual Real do_dequantize(const Int n) const;
+};
+
+}
+
+#include "Quantizer.tpp"
+#endif
