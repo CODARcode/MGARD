c80d12e365a2bf87772f17e364a1f30a6f121fb1
qliu21 <qing.liu@njit.edu>
2020-02-20 20:51:22 -0500

    Consine1d (#67)
    * added a simple 1d test.
    
    * format fix.
    
    * quick 1D code fix.

--- tests/simple1d/simple1d.cpp
+++ tests/simple1d/simple1d.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-  double step, result, tol = 0.001;
+  double step, result, tol = 0.0000001;
   int out_size;
-  const int npoints = 64;
+  const int npoints = 8;
   double v[npoints];
   unsigned char *compressed_data = 0;
 
@@ -33,8 +33,8 @@ int main(int argc, char **argv) {
       max_abserr = abserr;
     }
 
-    if (max < v[i]) {
-      max = v[i];
+    if (max < abs(v[i])) {
+      max = abs(v[i]);
     }
   }
 
