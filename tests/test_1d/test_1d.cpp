54ce49eafc88cb8bfb984f2186276b9138f79bb0
Gary Liu <qing.liu@njit.edu>
2020-02-28 23:07:43 -0500

    format fix.

--- tests/test_1d/test_1d.cpp
+++ tests/test_1d/test_1d.cpp
@@ -1,8 +1,8 @@
 #include "mgard_api.h"
 #include <cmath>
+#include <fstream>
 #include <iostream>
 #include <vector>
-#include<fstream>
 
 using namespace std;
 
@@ -17,24 +17,25 @@ int main(int argc, char **argv) {
   }
 
   ifstream datafile(argv[1], ios::in | ios::binary);
-  if (!datafile) cerr << "Can't open input file!";
+  if (!datafile)
+    cerr << "Can't open input file!";
 
   datafile.seekg(0, ios::end);
   size_t num_elements = datafile.tellg() / sizeof(double);
   datafile.seekg(0, ios::beg);
 
-  std::cout << "size = " << num_elements << "\n";
   std::vector<double> data(num_elements);
-  datafile.read(reinterpret_cast<char*>(&data[0]), num_elements*sizeof(double));
+  datafile.read(reinterpret_cast<char *>(&data[0]),
+                num_elements * sizeof(double));
 
-//  num_elements = num_elements / 2;
-  compressed_data = mgard_compress(0, data.data(), out_size, 1 , num_elements, 1, tol);
+  compressed_data =
+      mgard_compress(0, data.data(), out_size, 1, num_elements, 1, tol);
   cout << "Original size = " << num_elements * 8 << " out_size = " << out_size
        << " CR = " << num_elements * 8.0 / out_size << endl;
 
   double quantizer;
-  double *decompressed_data =
-      mgard_decompress(0, quantizer, compressed_data, out_size, 1, num_elements, 1);
+  double *decompressed_data = mgard_decompress(0, quantizer, compressed_data,
+                                               out_size, 1, num_elements, 1);
 
   double abserr = 0.0;
   double max_abserr = 0.0;
@@ -49,7 +50,7 @@ int main(int argc, char **argv) {
       max = abs(data[i]);
     }
   }
-cout << "max = " << max << "\n";
+
   cout << "The prescribed error = " << tol
        << " The achieved error = " << max_abserr / max << endl;
 
