c08e4e6dbe4f5b3bba71377334a1949882f1e9ae
Ben Whitney <whitneybe@ornl.gov>
2020-06-23 10:14:59 -0400

    Add `TensorLinearOperator` and tests.

--- tests/src/test_TensorLinearOperator.cpp
+++ tests/src/test_TensorLinearOperator.cpp
@@ -11,7 +11,12 @@
 
 namespace {
 
-class DiagonalOperator : public mgard::ConstituentLinearOperator<2, float> {
+template <std::size_t N>
+class DiagonalOperator : public mgard::ConstituentLinearOperator<N, float> {
+
+  using Real = float;
+  using CLO = mgard::ConstituentLinearOperator<N, float>;
+
 public:
   //! Constructor.
   //
@@ -20,27 +25,27 @@ public:
   //!\param dimension Index of the dimension in which the operator is to
   //! be applied.
   //!\param scalar Diagonal of the matrix.
-  DiagonalOperator(const mgard::TensorMeshHierarchy<2, float> &hierarchy,
+  DiagonalOperator(const mgard::TensorMeshHierarchy<N, Real> &hierarchy,
                    const std::size_t l, const std::size_t dimension,
-                   const std::vector<float> diagonal)
-      : ConstituentLinearOperator(hierarchy, l, dimension), diagonal(diagonal) {
+                   const std::vector<Real> diagonal)
+      : CLO(hierarchy, l, dimension), diagonal(diagonal) {
     if (diagonal.size() != this->dimension()) {
       throw std::invalid_argument("operator and mesh sizes don't match");
     }
   }
 
   //! Diagonal of the matrix.
-  const std::vector<float> diagonal;
+  const std::vector<Real> diagonal;
 
 private:
-  void do_operator_parentheses(const std::array<std::size_t, 2> multiindex,
-                               float *const v) const override {
-    std::array<std::size_t, 2> alpha = multiindex;
-    std::size_t &variable_index = alpha.at(dimension_);
-    const std::size_t N = dimension();
-    for (std::size_t i = 0; i < N; ++i) {
-      variable_index = indices.at(i);
-      hierarchy.at(v, alpha) *= diagonal.at(i);
+  void do_operator_parentheses(const std::array<std::size_t, N> multiindex,
+                               Real *const v) const override {
+    std::array<std::size_t, N> alpha = multiindex;
+    std::size_t &variable_index = alpha.at(CLO::dimension_);
+    const std::size_t M = CLO::dimension();
+    for (std::size_t i = 0; i < M; ++i) {
+      variable_index = CLO::indices.at(i);
+      CLO::hierarchy.at(v, alpha) *= diagonal.at(i);
     }
   }
 };
@@ -98,7 +103,9 @@ TEST_CASE("simple constituent operators", "[TensorLinearOperator]") {
     const mgard::TensorMeshHierarchy<2, float> hierarchy({3, 3});
     const std::array<float, 9> u_ = {-1, 5, -3, 0, 1, 4, 2, 3, -2};
     {
-      const DiagonalOperator A(hierarchy, 1, 1, {1.5, 0.5, 2.5});
+      const std::size_t l = 1;
+      const std::size_t dimension = 1;
+      const DiagonalOperator A(hierarchy, l, dimension, {1.5, 0.5, 2.5});
       std::array<float, 9> v_ = u_;
       float *const v = v_.data();
       A({0, 0}, v);
@@ -108,7 +115,9 @@ TEST_CASE("simple constituent operators", "[TensorLinearOperator]") {
       REQUIRE(v_ == expected);
     }
     {
-      const DiagonalOperator A(hierarchy, 0, 0, {-2, -1});
+      const std::size_t l = 0;
+      const std::size_t dimension = 0;
+      const DiagonalOperator A(hierarchy, l, dimension, {-2, -1});
       std::array<float, 9> v_ = u_;
       float *const v = v_.data();
       A({0, 0}, v);
@@ -120,12 +129,14 @@ TEST_CASE("simple constituent operators", "[TensorLinearOperator]") {
 
   SECTION("matrix constituent operators") {
     const mgard::TensorMeshHierarchy<2, float> hierarchy({5, 5});
+    const std::size_t l = 1;
     std::array<float, 25> u_ = {4, 0, -4, -5, -7, 3, 9, 3,  -3, 0, -10, 2, -5,
                                 5, 0, 4,  5,  -6, 4, 5, -9, -1, 0, 9,   -6};
     const std::array<std::array<float, 3>, 3> coefficients = {
         {{4, -8, 0}, {7, -6, 3}, {-2, -10, -10}}};
     {
-      const ThreeByThreeMatrix A(hierarchy, 1, 0, coefficients);
+      const std::size_t dimension = 0;
+      const ThreeByThreeMatrix A(hierarchy, l, dimension, coefficients);
       std::array<float, 25> v_ = u_;
       float *const v = v_.data();
       A({0, 2}, v);
@@ -136,13 +147,69 @@ TEST_CASE("simple constituent operators", "[TensorLinearOperator]") {
       REQUIRE(v_ == expected);
     }
     {
-      const ThreeByThreeMatrix A(hierarchy, 1, 1, coefficients);
+      const std::size_t dimension = 1;
+      const ThreeByThreeMatrix A(hierarchy, l, dimension, coefficients);
       std::array<float, 25> v_ = u_;
       float *const v = v_.data();
       A({0, 0}, v);
       const std::array<float, 25> expected = {48, 0,   31, -5, 102, 3, 9, 3, -3,
                                               0,  -10, 2,  -5, 5,   0, 4, 5, -6,
                                               4,  5,   -9, -1, 0,   9, -6};
+      REQUIRE(v_ == expected);
+    }
+  }
+}
+
+TEST_CASE("tensor products of simple constituent operators",
+          "[TensorLinearOperator]") {
+  {
+    const mgard::TensorMeshHierarchy<3, float> hierarchy({2, 2, 2});
+    const std::size_t l = 0;
+    const DiagonalOperator A(hierarchy, l, 0, {2, 1});
+    const DiagonalOperator B(hierarchy, l, 1, {1, 3});
+    const DiagonalOperator C(hierarchy, l, 2, {1, 5});
+    const mgard::TensorLinearOperator<3, float> M(hierarchy, l, {&A, &B, &C});
+
+    const std::array<float, 9> u_ = {0, 1, 9, 7, 3, 0, 1, 10};
+#if 0
+    (0, 0, 0): 0
+    (0, 0, 1): 1
+    (0, 1, 0): 9
+    (0, 1, 1): 7
+    (1, 0, 0): 3
+    (1, 0, 1): 0
+    (1, 1, 0): 1
+    (1, 1, 1): 10
+#endif
+    {
+      std::array<float, 9> v_ = u_;
+      float *const v = v_.data();
+      M(v);
+      const std::array<float, 9> expected = {0, 10, 54, 210, 3, 0, 3, 150};
+      REQUIRE(v_ == expected);
+    }
+  }
+
+  {
+    const mgard::TensorMeshHierarchy<2, float> hierarchy({3, 3});
+    const std::size_t l = 1;
+
+    const std::array<std::array<float, 3>, 3> A_ = {
+        {{1, 1, 2}, {0, 1, 3}, {0, 0, 1}}};
+    const ThreeByThreeMatrix A(hierarchy, l, 0, A_);
+
+    const std::array<std::array<float, 3>, 3> B_ = {
+        {{1, 4, 2}, {0, 1, 1}, {0, 0, 1}}};
+    const ThreeByThreeMatrix B(hierarchy, l, 1, B_);
+
+    const mgard::TensorLinearOperator<2, float> M(hierarchy, l, {&A, &B});
+
+    const std::array<float, 9> u_ = {1, 0, 2, 0, 0, 0, 4, 0, 1};
+    {
+      std::array<float, 9> v_ = u_;
+      float *const v = v_.data();
+      M(v);
+      const std::array<float, 9> expected = {17, 4, 4, 18, 3, 3, 6, 1, 1};
       REQUIRE(v_ == expected);
     }
   }
