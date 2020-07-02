e07c93c735e36bf9d6b201ad695d07ff97774271
Ben Whitney <whitneybe@ornl.gov>
2020-07-02 13:48:59 -0400

    Add `TensorProlongationAddition` and tests.

--- include/TensorProlongation.hpp
+++ include/TensorProlongation.hpp
@@ -43,6 +43,31 @@ private:
                           Real *const v) const override;
 };
 
+//! Prolongation–addition (interpolate the values on the 'old' nodes to the
+//! 'new' nodes and add (not overwrite) for tensor products of continuous
+//! piecewise linear functions defined on a Cartesian product mesh hierarchy.
+//!
+//! *Important:* This operator only works as expected when the values on the
+//! 'new' nodes are all zero to start.
+template <std::size_t N, typename Real>
+class TensorProlongationAddition : public TensorLinearOperator<N, Real> {
+public:
+  //! Constructor.
+  //!
+  //!\param hierarchy Mesh hierarchy on which the functions are defined.
+  //!\param l Index of the mesh on which the prolongation–addition is to be
+  //! applied.
+  TensorProlongationAddition(const TensorMeshHierarchy<N, Real> &hierarchy,
+                             const std::size_t l);
+
+private:
+  using TLO = TensorLinearOperator<N, Real>;
+
+  //! Constituent prolongation–additions for each dimension.
+  const std::array<ConstituentProlongationAddition<N, Real>, N>
+      prolongation_additions;
+};
+
 } // namespace mgard
 
 #include "TensorProlongation.tpp"
