0982c736b2fd642a2cab5639bc92518b6832586b
Ben Whitney <whitneybe@ornl.gov>
2020-07-07 15:11:01 -0400

    Add `TensorRestriction` and tests.

--- tests/src/test_TensorRestriction.cpp
+++ tests/src/test_TensorRestriction.cpp
@@ -5,7 +5,9 @@
 #include "testing_random.hpp"
 #include "testing_utilities.hpp"
 
+#include "TensorMassMatrix.hpp"
 #include "TensorMeshHierarchy.hpp"
+#include "TensorProlongation.hpp"
 #include "TensorRestriction.hpp"
 #include "utilities.hpp"
 
@@ -95,5 +97,79 @@ TEST_CASE("constituent restrictions", "[TensorRestriction]") {
       }
       REQUIRE(thrown);
     }
+  }
+}
+
+namespace {
+
+// Prolongate a function on the coarse mesh to the fine mesh, project back down
+// to the coarse mesh, and compare.
+template <std::size_t N, typename Real>
+void test_tensor_projection_identity(std::default_random_engine &generator,
+                                     const std::array<std::size_t, N> shape) {
+  std::uniform_real_distribution<Real> node_spacing_distribution(1, 1.5);
+  std::uniform_real_distribution<Real> polynomial_coefficient_distribution(
+      -0.5, 1.25);
+  const mgard::TensorMeshHierarchy<N, Real> hierarchy =
+      hierarchy_with_random_spacing(generator, node_spacing_distribution,
+                                    shape);
+
+  std::vector<Real> u_(hierarchy.ndof());
+  Real *const u = u_.data();
+
+  for (std::size_t l = hierarchy.L; l > 0; --l) {
+    const MultilinearPolynomial<Real, N> p(generator,
+                                           polynomial_coefficient_distribution);
+    for (const mgard::SituatedCoefficient<N, Real> coeff :
+         hierarchy.on_nodes(u, l)) {
+      *coeff.value = 0;
+    }
+    for (const mgard::SituatedCoefficient<N, Real> coeff :
+         hierarchy.on_nodes(u, l - 1)) {
+      *coeff.value = p(coeff.coordinates);
+    }
+
+    const mgard::TensorProlongationAddition<N, Real> PA(hierarchy, l);
+    const mgard::TensorRestriction<N, Real> R(hierarchy, l);
+    const mgard::TensorMassMatrixInverse<N, Real> m_inv(hierarchy, l - 1);
+    const mgard::TensorMassMatrix<N, Real> M(hierarchy, l);
+
+    PA(u);
+    M(u);
+    R(u);
+    m_inv(u);
+
+    TrialTracker tracker;
+    for (const mgard::SituatedCoefficient<N, Real> coeff :
+         hierarchy.on_nodes(u, l - 1)) {
+      tracker += hierarchy.at(u, coeff.multiindex) == Approx(*coeff.value);
+    }
+    REQUIRE(tracker);
+  }
+}
+
+} // namespace
+
+TEST_CASE("tensor product restrictions", "[TensorRestriction]") {
+  {
+    const mgard::TensorMeshHierarchy<2, double> hierarchy(
+        {3, 3}, {{{0, 0.5, 1}, {-1, -0.5, 1}}});
+    const std::array<double, 9> u_ = {6, 0, 7, -6, -10, 8, -6, 3, 9};
+    const std::size_t l = 1;
+    const mgard::TensorRestriction<2, double> R(hierarchy, l);
+    const std::array<double, 9> expected = {-0.75, -5,    9.75, -13.5, -10,
+                                            5.5,   -10.5, -2,   12.5};
+    std::array<double, 9> v_ = u_;
+    double *const v = v_.data();
+    R(v);
+    REQUIRE(v_ == expected);
+  }
+
+  {
+    std::default_random_engine generator(445624);
+    test_tensor_projection_identity<1, float>(generator, {129});
+    test_tensor_projection_identity<2, double>(generator, {65, 65});
+    test_tensor_projection_identity<3, float>(generator, {33, 9, 33});
+    test_tensor_projection_identity<4, double>(generator, {9, 9, 17, 17});
   }
 }
