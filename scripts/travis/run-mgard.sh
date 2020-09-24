43374e829c301c7163a6cac07758d9e3dd9b2e20
Ben Whitney <whitneybe@ornl.gov>
2020-09-24 10:51:53 -0400

    Move `cosine` test to Catch2 test case.

--- scripts/travis/run-mgard.sh
+++ scripts/travis/run-mgard.sh
@@ -13,16 +13,6 @@ fi
 
 cd ${SOURCE_DIR}
 
-./build/bin/cosine
-
-if [ $? -eq 0 ]
-then
-  echo "Successfully compressed/decompressed cosine"
-else
-  echo "Error in compressing/decompressing cosine" >&2
-  exit 1
-fi
-
 ./build/bin/constant3d
 
 if [ $? -eq 0 ]
