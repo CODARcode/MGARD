ca5b4c8220ffd1b2d8eff477a4a8649dacb09b79
Ben Whitney <whitneybe@ornl.gov>
2020-10-22 11:40:28 -0400

    Enable iteration over nodes in shuffled order.

--- include/TensorMeshHierarchyIteration.hpp
+++ include/TensorMeshHierarchyIteration.hpp
@@ -168,6 +168,8 @@ private:
 };
 
 //! Nodes of a particular level in a mesh hierarchy.
+//!
+//! This range yields the nodes ordered by physical location.
 template <std::size_t N, typename Real> class TensorNodeRange {
 public:
   //! Constructor.
@@ -245,7 +247,9 @@ public:
   reference operator*() const;
 
   //! View of nodes being iterated over.
-  const TensorNodeRange &iterable;
+  //!
+  //! A pointer rather than a reference so we can assign.
+  TensorNodeRange const *iterable;
 
 private:
   //! Underlying multiindex iterator.
@@ -253,6 +257,8 @@ private:
 };
 
 //! 'Reserved' nodes of a particular level in a mesh hierarchy.
+//!
+//! This range yields the nodes ordered by physical location.
 //!
 //! The difference between `TensorNodeRange` and `TensorReservedNodeRange` is
 //! that the nodes of the latter only think of nodes present at their
@@ -351,6 +357,97 @@ private:
   //! Underlying range iterator on the level being iterated over. This is just
   //! the last entry of `inners`.
   typename TensorNodeRange<N, Real>::iterator inner_finest;
+};
+
+//! Nodes of a particular level in a mesh hierarchy.
+//!
+//! This range yields the nodes ordered by level, matching the coefficient
+//! ordering produced by `shuffle`.
+template <std::size_t N, typename Real> class ShuffledTensorNodeRange {
+public:
+  //! Constructor.
+  //!
+  //!\param hierarchy Associated mesh hierarchy.
+  //!\param l Index of the mesh level to be iterated over.
+  ShuffledTensorNodeRange(const TensorMeshHierarchy<N, Real> &hierarchy,
+                          const std::size_t l);
+
+  // Forward declaration.
+  class iterator;
+
+  //! Return an iterator to the beginning of the nodes.
+  iterator begin() const;
+
+  //! Return an iterator to the end of the nodes.
+  iterator end() const;
+
+  //! Equality comparison.
+  bool operator==(const ShuffledTensorNodeRange &other) const;
+
+  //! Inequality comparison.
+  bool operator!=(const ShuffledTensorNodeRange &other) const;
+
+  //! Associated mesh hierarchy.
+  const TensorMeshHierarchy<N, Real> &hierarchy;
+
+  //! Node ranges from the coarsest level up to the level being iterated over.
+  const std::vector<TensorNodeRange<N, Real>> ranges;
+
+private:
+  //! Index of the level being iterated over.
+  //!
+  //! This is only stored so we can avoid comparing `ranges` in the
+  //! (in)equality comparison operators.
+  const std::size_t l;
+};
+
+//! Iterator over the nodes of a mesh in a mesh hierarchy.
+template <std::size_t N, typename Real>
+class ShuffledTensorNodeRange<N, Real>::iterator {
+public:
+  //! Category of the iterator.
+  using iterator_category = std::input_iterator_tag;
+  //! Type iterated over.
+  using value_type = TensorNode<N>;
+  //! Type for distance between iterators.
+  using difference_type = std::ptrdiff_t;
+  //! Pointer to `value_type`.
+  using pointer = value_type *;
+  //! Type returned by the dereference operator.
+  using reference = value_type;
+
+  //! Constructor.
+  //!
+  //!\param iterable View of nodes to be iterated over.
+  //!\param ell Index of mesh currently being iterated over.
+  //!\param inner Underlying range iterator.
+  iterator(const ShuffledTensorNodeRange &iterable, const std::size_t ell,
+           const typename TensorNodeRange<N, Real>::iterator inner);
+
+  //! Equality comparison.
+  bool operator==(const iterator &other) const;
+
+  //! Inequality comparison.
+  bool operator!=(const iterator &other) const;
+
+  //! Preincrement.
+  iterator &operator++();
+
+  //! Postincrement.
+  iterator operator++(int);
+
+  //! Dereference.
+  reference operator*() const;
+
+  //! View of nodes being iterated over.
+  const ShuffledTensorNodeRange &iterable;
+
+private:
+  //! Index of mesh currently being iterated over.
+  std::size_t ell;
+
+  //! Underlying range iterator.
+  typename TensorNodeRange<N, Real>::iterator inner;
 };
 
 } // namespace mgard
