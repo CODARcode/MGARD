7f4e72cce7b1d3528bbbde1230218d6a0c39bc00
Ben Whitney <whitneybe@ornl.gov>
2020-11-19 13:16:24 -0500

    Capitalize package names consistently.

--- cmake/Findarchive.cmake
+++ cmake/Findarchive.cmake
@@ -5,49 +5,49 @@
 #
 # This module defines the following variables:
 #
-#     ARCHIVE_FOUND        - Whether archive was found.
-#     ARCHIVE_INCLUDE_DIRS - Location of archive headers.
-#     ARCHIVE_LIBRARIES    - archive library and other required libraries.
+#     archive_FOUND        - Whether archive was found.
+#     archive_INCLUDE_DIRS - Location of archive headers.
+#     archive_LIBRARIES    - archive library and other required libraries.
 
-set(ARCHIVE_LIBRARIES_PATHS ~/.local/lib /usr/local/lib /usr/lib)
-set(ARCHIVE_INCLUDE_DIRS_PATHS ~/.local/include /usr/local/include /usr/include)
+set(archive_LIBRARIES_PATHS ~/.local/lib /usr/local/lib /usr/lib)
+set(archive_INCLUDE_DIRS_PATHS ~/.local/include /usr/local/include /usr/include)
 
-set(ARCHIVE_LIBRARIES_ERROR_MESSAGE "Could not find archive library.")
-set(ARCHIVE_INCLUDE_DIRS_ERROR_MESSAGE "archive headers not found.")
-set(ARCHIVE_FOUND_ERROR_MESSAGE "archive not found.")
+set(archive_LIBRARIES_ERROR_MESSAGE "Could not find archive library.")
+set(archive_INCLUDE_DIRS_ERROR_MESSAGE "archive headers not found.")
+set(archive_FOUND_ERROR_MESSAGE "archive not found.")
 
-set(ARCHIVE_FOUND TRUE)
+set(archive_FOUND TRUE)
 
-if(ARCHIVE_FIND_QUIETLY OR NOT ARCHIVE_FIND_REQUIRED)
-	find_path(ARCHIVE_INCLUDE_DIRS NAMES archive.h archive_entry.h PATHS ${ARCHIVE_INCLUDE_DIRS_PATHS})
+if(archive_FIND_QUIETLY OR NOT archive_FIND_REQUIRED)
+	find_path(archive_INCLUDE_DIRS NAMES archive.h archive_entry.h PATHS ${archive_INCLUDE_DIRS_PATHS})
 else()
-	find_path(ARCHIVE_INCLUDE_DIRS NAMES archive.h archive_entry.h PATHS ${ARCHIVE_INCLUDE_DIRS_PATHS} REQUIRED)
+	find_path(archive_INCLUDE_DIRS NAMES archive.h archive_entry.h PATHS ${archive_INCLUDE_DIRS_PATHS} REQUIRED)
 endif()
 
-if (ARCHIVE_INCLUDE_DIRS STREQUAL "ARCHIVE_INCLUDE_DIRS-NOTFOUND")
-	set(ARCHIVE_FOUND FALSE)
-	if(NOT ARCHIVE_FIND_QUIETLY)
-		message(STATUS ${ARCHIVE_INCLUDE_DIRS_ERROR_MESSAGE})
+if (archive_INCLUDE_DIRS STREQUAL "archive_INCLUDE_DIRS-NOTFOUND")
+	set(archive_FOUND FALSE)
+	if(NOT archive_FIND_QUIETLY)
+		message(STATUS ${archive_INCLUDE_DIRS_ERROR_MESSAGE})
 	endif()
 endif()
 
-if (ARCHIVE_FIND_QUIETLY OR NOT ARCHIVE_FIND_REQUIRED)
-	find_library(ARCHIVE_LIBRARIES archive PATHS ${ARCHIVE_LIBRARIES_PATHS})
+if (archive_FIND_QUIETLY OR NOT archive_FIND_REQUIRED)
+	find_library(archive_LIBRARIES archive PATHS ${archive_LIBRARIES_PATHS})
 else()
-	find_library(ARCHIVE_LIBRARIES archive PATHS ${ARCHIVE_LIBRARIES_PATHS} REQUIRED)
+	find_library(archive_LIBRARIES archive PATHS ${archive_LIBRARIES_PATHS} REQUIRED)
 endif()
 
-if(ARCHIVE_LIBRARIES STREQUAL "ARCHIVE_LIBRARIES-NOTFOUND")
-	set(ARCHIVE_FOUND FALSE)
-	if(NOT ARCHIVE_FIND_QUIETLY)
-		message(STATUS ${ARCHIVE_LIBRARIES_ERROR_MESSAGE})
+if(archive_LIBRARIES STREQUAL "archive_LIBRARIES-NOTFOUND")
+	set(archive_FOUND FALSE)
+	if(NOT archive_FIND_QUIETLY)
+		message(STATUS ${archive_LIBRARIES_ERROR_MESSAGE})
 	endif()
 endif()
 
-if (NOT ARCHIVE_FOUND)
-	if(ARCHIVE_FIND_REQUIRED)
-		message(FATAL_ERROR ${ARCHIVE_FOUND_ERROR_MESSAGE})
+if (NOT archive_FOUND)
+	if(archive_FIND_REQUIRED)
+		message(FATAL_ERROR ${archive_FOUND_ERROR_MESSAGE})
 	else()
-		message(STATUS ${ARCHIVE_FOUND_ERROR_MESSAGE})
+		message(STATUS ${archive_FOUND_ERROR_MESSAGE})
 	endif()
 endif()
--- cmake/Findyaml-cpp.cmake
+++ cmake/Findyaml-cpp.cmake
@@ -5,49 +5,49 @@
 #
 # This module defines the following variables:
 #
-#     YAML-CPP_FOUND        - Whether yaml-cpp was found.
-#     YAML-CPP_INCLUDE_DIRS - Location of yaml-cpp headers.
-#     YAML-CPP_LIBRARIES    - yaml-cpp library and other required libraries.
+#     yaml-cpp_FOUND        - Whether yaml-cpp was found.
+#     yaml-cpp_INCLUDE_DIRS - Location of yaml-cpp headers.
+#     yaml-cpp_LIBRARIES    - yaml-cpp library and other required libraries.
 
-set(YAML-CPP_LIBRARIES_PATHS ~/.local/lib /usr/local/lib /usr/lib)
-set(YAML-CPP_INCLUDE_DIRS_PATHS ~/.local/include /usr/local/include /usr/include)
+set(yaml-cpp_LIBRARIES_PATHS ~/.local/lib /usr/local/lib /usr/lib)
+set(yaml-cpp_INCLUDE_DIRS_PATHS ~/.local/include /usr/local/include /usr/include)
 
-set(YAML-CPP_LIBRARIES_ERROR_MESSAGE "Could not find yaml-cpp library.")
-set(YAML-CPP_INCLUDE_DIRS_ERROR_MESSAGE "yaml-cpp headers not found.")
-set(YAML-CPP_FOUND_ERROR_MESSAGE "yaml-cpp not found.")
+set(yaml-cpp_LIBRARIES_ERROR_MESSAGE "Could not find yaml-cpp library.")
+set(yaml-cpp_INCLUDE_DIRS_ERROR_MESSAGE "yaml-cpp headers not found.")
+set(yaml-cpp_FOUND_ERROR_MESSAGE "yaml-cpp not found.")
 
-set(YAML-CPP_FOUND TRUE)
+set(yaml-cpp_FOUND TRUE)
 
-if(YAML-CPP_FIND_QUIETLY OR NOT YAML-CPP_FIND_REQUIRED)
-	find_path(YAML-CPP_INCLUDE_DIRS yaml-cpp PATHS ${YAML-CPP_INCLUDE_DIRS_PATHS})
+if(yaml-cpp_FIND_QUIETLY OR NOT yaml-cpp_FIND_REQUIRED)
+	find_path(yaml-cpp_INCLUDE_DIRS yaml-cpp PATHS ${yaml-cpp_INCLUDE_DIRS_PATHS})
 else()
-	find_path(YAML-CPP_INCLUDE_DIRS yaml-cpp PATHS ${YAML-CPP_INCLUDE_DIRS_PATHS} REQUIRED)
+	find_path(yaml-cpp_INCLUDE_DIRS yaml-cpp PATHS ${yaml-cpp_INCLUDE_DIRS_PATHS} REQUIRED)
 endif()
 
-if (YAML-CPP_INCLUDE_DIRS STREQUAL "YAML-CPP_INCLUDE_DIRS-NOTFOUND")
-	set(YAML-CPP_FOUND FALSE)
-	if(NOT YAML-CPP_FIND_QUIETLY)
-		message(STATUS ${YAML-CPP_INCLUDE_DIRS_ERROR_MESSAGE})
+if (yaml-cpp_INCLUDE_DIRS STREQUAL "yaml-cpp_INCLUDE_DIRS-NOTFOUND")
+	set(yaml-cpp_FOUND FALSE)
+	if(NOT yaml-cpp_FIND_QUIETLY)
+		message(STATUS ${yaml-cpp_INCLUDE_DIRS_ERROR_MESSAGE})
 	endif()
 endif()
 
-if (YAML-CPP_FIND_QUIETLY OR NOT YAML-CPP_FIND_REQUIRED)
-	find_library(YAML-CPP_LIBRARIES yaml-cpp PATHS ${YAML-CPP_LIBRARIES_PATHS})
+if (yaml-cpp_FIND_QUIETLY OR NOT yaml-cpp_FIND_REQUIRED)
+	find_library(yaml-cpp_LIBRARIES yaml-cpp PATHS ${yaml-cpp_LIBRARIES_PATHS})
 else()
-	find_library(YAML-CPP_LIBRARIES yaml-cpp PATHS ${YAML-CPP_LIBRARIES_PATHS} REQUIRED)
+	find_library(yaml-cpp_LIBRARIES yaml-cpp PATHS ${yaml-cpp_LIBRARIES_PATHS} REQUIRED)
 endif()
 
-if(YAML-CPP_LIBRARIES STREQUAL "YAML-CPP_LIBRARIES-NOTFOUND")
-	set(YAML-CPP_FOUND FALSE)
-	if(NOT YAML-CPP_FIND_QUIETLY)
-		message(STATUS ${YAML-CPP_LIBRARIES_ERROR_MESSAGE})
+if(yaml-cpp_LIBRARIES STREQUAL "yaml-cpp_LIBRARIES-NOTFOUND")
+	set(yaml-cpp_FOUND FALSE)
+	if(NOT yaml-cpp_FIND_QUIETLY)
+		message(STATUS ${yaml-cpp_LIBRARIES_ERROR_MESSAGE})
 	endif()
 endif()
 
-if (NOT YAML-CPP_FOUND)
-	if(YAML-CPP_FIND_REQUIRED)
-		message(FATAL_ERROR ${YAML-CPP_FOUND_ERROR_MESSAGE})
+if (NOT yaml-cpp_FOUND)
+	if(yaml-cpp_FIND_REQUIRED)
+		message(FATAL_ERROR ${yaml-cpp_FOUND_ERROR_MESSAGE})
 	else()
-		message(STATUS ${YAML-CPP_FOUND_ERROR_MESSAGE})
+		message(STATUS ${yaml-cpp_FOUND_ERROR_MESSAGE})
 	endif()
 endif()
