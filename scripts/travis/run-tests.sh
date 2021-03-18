b35fcba667066d366e6a683d5ea7984c5cd19200
Ben Whitney <whitneybe@ornl.gov>
2021-03-18 12:53:16 -0400

    Simplify Cppcheck Travis script.

--- scripts/travis/run-tests.sh
+++ scripts/travis/run-tests.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -z "${SOURCE_DIR}" ]
-then
-  echo "Error: SOURCE_DIR is empty or undefined"
-  exit 1
-fi
-if [ -z "${COMMIT_RANGE}" ]
-then
-  echo "Error: COMMIT_RANGE is empty or undefined"
-  exit 1
-fi
-
-cd ${SOURCE_DIR}
-
+cd "${TRAVIS_BUILD_DIR}"
 build/bin/tests
 
 if [ $? -eq 0 ]
