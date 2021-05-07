0ad646634d9b55f8ccf4542fbbad2549d09766ea
Ben Whitney <whitneybe@ornl.gov>
2021-05-07 12:31:49 -0400

    Add parsers for command line interface.

---
+++ include/cmdline.hpp
@@ -0,0 +1,144 @@
+#ifndef CMDLINE_HPP
+#define CMDLINE_HPP
+//!\file
+//!\brief Parsers for the executable command line interface.
+
+#include <list>
+#include <map>
+#include <string>
+#include <vector>
+
+#include <tclap/ArgException.h>
+#include <tclap/CmdLineInterface.h>
+#include <tclap/HelpVisitor.h>
+#include <tclap/SwitchArg.h>
+#include <tclap/UnlabeledValueArg.h>
+#include <tclap/ValuesConstraint.h>
+#include <tclap/VersionVisitor.h>
+
+// Base class for sub- and supercommand parsers.
+class BaseCmdLine : public TCLAP::CmdLineInterface {
+public:
+  //! Constructor.
+  //!
+  //!\param version Version of the command.
+  //!\param message Message describing the command.
+  //!\param output Outputter to use for the command.
+  BaseCmdLine(const std::string &version, const std::string &message,
+              TCLAP::CmdLineOutput *const output);
+
+  virtual void add(TCLAP::Arg &a) override;
+
+  virtual void add(TCLAP::Arg *a) override;
+
+  virtual void xorAdd(TCLAP::Arg &a, TCLAP::Arg &b) override;
+
+  virtual void xorAdd(std::vector<TCLAP::Arg *> &xors) override;
+
+  virtual TCLAP::CmdLineOutput *getOutput() override;
+
+  virtual void setOutput(TCLAP::CmdLineOutput *co) override;
+
+  virtual std::string &getVersion() override;
+
+  virtual std::string &getProgramName() override;
+
+  virtual std::list<TCLAP::Arg *> &getArgList() override;
+
+  virtual TCLAP::XorHandler &getXorHandler() override;
+
+  virtual char getDelimiter() override;
+
+  virtual std::string &getMessage() override;
+
+  virtual void reset() override;
+
+protected:
+  //! Name used to call the program.
+  std::string _programName{"[program name not set]"};
+
+  //! Version of the command.
+  std::string _version{"[version number not set]"};
+
+  //! Message describing the command.
+  std::string _message{"[message not set]"};
+
+  //! Outputter of the command.
+  TCLAP::CmdLineOutput *_output;
+
+  //! Visitor for the '--help' switch.
+  TCLAP::HelpVisitor _helpVisitor;
+
+  //! Argument for the '--help' switch.
+  TCLAP::SwitchArg _helpArg;
+
+  //! Arguments of the command.
+  std::list<TCLAP::Arg *> _argList;
+};
+
+//! Subcommand parser.
+class SubCmdLine : public BaseCmdLine {
+public:
+  //! Constructor.
+  //!
+  //!\param version Version of the subcommand.
+  //!\param message Message describing the subcommand.
+  SubCmdLine(const std::string &version, const std::string &message);
+
+  //! Destructor.
+  ~SubCmdLine();
+
+  virtual void parse(int argc, const char *const *const argv) override;
+
+  //! Parse the subcommand arguments.
+  //!
+  //!\param arg Subcommand arguments.
+  void parse(std::vector<std::string> &args);
+
+  virtual bool hasHelpAndVersion() override;
+};
+
+//! Supercommand parser.
+class SuperCmdLine : public BaseCmdLine {
+public:
+  //! Constructor.
+  //!
+  //!\param version Version of the supercommand.
+  //!\param message Message describing the supercommand.
+  //!\param subcommands Associated subcommands.
+  SuperCmdLine(const std::string &version, const std::string &message,
+               std::map<std::string, SubCmdLine *> &subcommands);
+
+  //! Destructor.
+  ~SuperCmdLine();
+
+  virtual void parse(int argc, const char *const *const argv) override;
+
+  //! Parse the supercommand arguments.
+  //!
+  //!\param arg Supercommand arguments.
+  void parse(std::vector<std::string> &args);
+
+  virtual bool hasHelpAndVersion() override;
+
+private:
+  //! Visitor for the '--version' switch.
+  TCLAP::VersionVisitor _versionVisitor;
+
+  //! Argument for the '--version' switch.
+  TCLAP::SwitchArg _versionArg;
+
+  //! Associated subcommands.
+  std::map<std::string, SubCmdLine *> _subcommands;
+
+  //! Names of the subcommands.
+  std::vector<std::string> _subcommandNames;
+
+  //! Constraint for the subcommand name.
+  TCLAP::ValuesConstraint<std::string> _subcommandConstraint;
+
+  //! Argument for the subcommand.
+  TCLAP::UnlabeledValueArg<std::string> _subcommandArg;
+};
+
+#endif
---
+++ src/cmdline.cpp
@@ -0,0 +1,197 @@
+#include "cmdline.hpp"
+
+#include <cassert>
+
+#include <stdexcept>
+
+#include <tclap/ArgException.h>
+
+#include "output.hpp"
+
+BaseCmdLine::BaseCmdLine(const std::string &version, const std::string &message,
+                         TCLAP::CmdLineOutput *const output)
+    : _version(version), _message(message), _output(output),
+      // Adapted from `tclap/CmdLine.h`.
+      _helpVisitor(this, &_output),
+      _helpArg("h", "help", "Displays usage information and exits.", false,
+               &_helpVisitor) {
+  // This doesn't accomplish anything, but in general I'd like to use the
+  // 'official' setter member functions.
+  setOutput(_output);
+}
+
+void BaseCmdLine::add(TCLAP::Arg &a) { return add(&a); }
+
+void BaseCmdLine::add(TCLAP::Arg *a) { a->addToList(_argList); }
+
+void BaseCmdLine::xorAdd(TCLAP::Arg &a, TCLAP::Arg &b) {
+  throw std::runtime_error("not implemented");
+}
+
+void BaseCmdLine::xorAdd(std::vector<TCLAP::Arg *> &xors) {
+  throw std::runtime_error("not implemented");
+}
+
+TCLAP::CmdLineOutput *BaseCmdLine::getOutput() { return _output; }
+
+void BaseCmdLine::setOutput(TCLAP::CmdLineOutput *co) { _output = co; }
+
+std::string &BaseCmdLine::getVersion() { return _version; }
+
+std::string &BaseCmdLine::getProgramName() { return _programName; }
+
+std::list<TCLAP::Arg *> &BaseCmdLine::getArgList() { return _argList; }
+
+TCLAP::XorHandler &BaseCmdLine::getXorHandler() {
+  throw std::runtime_error("not implemented");
+}
+
+char BaseCmdLine::getDelimiter() { return ' '; }
+
+std::string &BaseCmdLine::getMessage() { return _message; }
+
+void BaseCmdLine::reset() { throw std::runtime_error("not implemented"); }
+
+SubCmdLine::SubCmdLine(const std::string &version, const std::string &message)
+    : BaseCmdLine(version, message, new SubOutput()) {
+  add(_helpArg);
+
+  setOutput(_output);
+}
+
+SubCmdLine::~SubCmdLine() { delete _output; }
+
+void SubCmdLine::parse(int argc, const char *const *const argv) {
+  std::vector<std::string> args(argv, argv + argc);
+  return parse(args);
+}
+
+void SubCmdLine::parse(std::vector<std::string> &args) {
+  try {
+    if (args.empty()) {
+      throw TCLAP::CmdLineParseException("Program name not provided.", "");
+    }
+    _programName = args.front();
+
+    if (args.size() < 2) {
+      throw TCLAP::ExitException(0);
+    }
+
+    int N = args.size();
+    for (int i = 1; i < N; ++i) {
+      bool matched = false;
+      for (TCLAP::Arg *p : _argList) {
+        TCLAP::Arg &arg = *p;
+        if (arg.processArg(&i, args)) {
+          matched = true;
+          break;
+        }
+      }
+      if (not matched) {
+        std::cerr << "Couldn't find match for argument" << std::endl;
+        throw TCLAP::CmdLineParseException("Couldn't find match for argument",
+                                           args[i]);
+      }
+    }
+    for (TCLAP::Arg *p : _argList) {
+      TCLAP::Arg &arg = *p;
+      if (arg.isRequired() and not arg.isSet()) {
+        throw TCLAP::CmdLineParseException("Required argument not provided",
+                                           arg.toString());
+      }
+    }
+  } catch (TCLAP::ArgException &e) {
+    try {
+      _output->failure(*this, e);
+    } catch (TCLAP::ExitException &e) {
+      std::exit(e.getExitStatus());
+    }
+  } catch (TCLAP::ExitException &e) {
+    std::exit(e.getExitStatus());
+  }
+}
+
+bool SubCmdLine::hasHelpAndVersion() { return false; }
+
+namespace {
+
+std::vector<std::string>
+subcommandNames(const std::map<std::string, SubCmdLine *> &subcommands) {
+  std::vector<std::string> names;
+  names.reserve(subcommands.size());
+  for (const auto &[key, _] : subcommands) {
+    names.push_back(key);
+  }
+  return names;
+}
+
+} // namespace
+
+SuperCmdLine::SuperCmdLine(const std::string &version,
+                           const std::string &message,
+                           std::map<std::string, SubCmdLine *> &subcommands)
+    : BaseCmdLine(version, message, new SuperOutput()),
+      _versionVisitor(this, &_output),
+      _versionArg("", "version", "Displays version information and exits.",
+                  false, &_versionVisitor),
+      _subcommands(subcommands), _subcommandNames(subcommandNames(subcommands)),
+      _subcommandConstraint(_subcommandNames),
+      _subcommandArg("subcommand", "Operation to perform.", true,
+                     "[no subcommand provided]", &_subcommandConstraint) {
+  add(_helpArg);
+  add(_versionArg);
+  add(_subcommandArg);
+}
+
+SuperCmdLine::~SuperCmdLine() { delete _output; }
+
+void SuperCmdLine::parse(int argc, const char *const *const argv) {
+  std::vector<std::string> args(argv, argv + argc);
+  return parse(args);
+}
+
+// Adapted from `TCLAP::CmdLine::parse`.
+void SuperCmdLine::parse(std::vector<std::string> &args) {
+  try {
+    if (args.empty()) {
+      throw TCLAP::CmdLineParseException("Program name not provided.", "");
+    }
+    _programName = args.front();
+
+    if (args.size() < 2) {
+      throw TCLAP::ExitException(0);
+    }
+
+    int i = 1;
+
+    _helpArg.processArg(&i, args);
+    // `_helpVisitor` should force an exit if the argument matches.
+    _versionArg.processArg(&i, args);
+    // `_versionVisitor` should force an exit if the argument matches.
+
+    // `SwitchArg::processArg` doesn't change `i` (as far as I can tell), so
+    // its value will still be `1`.
+    if (not _subcommandArg.processArg(&i, args)) {
+      throw TCLAP::CmdLineParseException("Couldn't find match for argument",
+                                         args[i]);
+    } else {
+      std::map<std::string, SubCmdLine *>::const_iterator p =
+          _subcommands.find(_subcommandArg.getValue());
+      assert(p != _subcommands.end());
+      assert(i == 1);
+      args.at(1) = args.at(0) + " " + args.at(1);
+      args.erase(args.begin());
+      return p->second->parse(args);
+    }
+  } catch (TCLAP::ArgException &e) {
+    try {
+      _output->failure(*this, e);
+    } catch (TCLAP::ExitException &e) {
+      std::exit(e.getExitStatus());
+    }
+  } catch (TCLAP::ExitException &e) {
+    std::exit(e.getExitStatus());
+  }
+}
+
+bool SuperCmdLine::hasHelpAndVersion() { return true; }
