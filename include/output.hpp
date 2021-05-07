85c8a36778db79326f7bdf90d18f54e469895a10
Ben Whitney <whitneybe@ornl.gov>
2021-05-07 12:31:39 -0400

    Add outputters for command line interface.

---
+++ include/output.hpp
@@ -0,0 +1,93 @@
+#ifndef OUTPUT_HPP
+#define OUTPUT_HPP
+//!\file
+//!\brief Outputters for the executable command line interface.
+
+#include <ostream>
+#include <sstream>
+#include <string>
+
+#include <tclap/ArgException.h>
+#include <tclap/CmdLineInterface.h>
+#include <tclap/CmdLineOutput.h>
+#include <tclap/StdOutput.h>
+
+#include "cmdline.hpp"
+
+//! Printing function object making `TCLAP::StdOutput::spacePrint` accessible.
+class SpacePrinter : private TCLAP::StdOutput {
+public:
+  //! Output a wrapped string to a stream.
+  //!
+  //!\param os Stream to which to output the wrapped string.
+  //!\param s String to wrap and output.
+  //!\param maxWidth Width to which to wrap the string.
+  //!\param indentSpaces Number of spaces with which to indent the output.
+  //!\param secondLineOffset Number of additional spaces with which to indent
+  //! all lines of the output except the first.
+  void operator()(std::ostream &os, const std::string &s, const int maxWidth,
+                  const int indentSpaces, const int secondLineOffset) const;
+};
+
+//! Base class for sub- and supercommand outputs.
+class BaseOutput : public TCLAP::CmdLineOutput {
+public:
+  virtual void usage(TCLAP::CmdLineInterface &c) override;
+
+  virtual void failure(TCLAP::CmdLineInterface &c,
+                       TCLAP::ArgException &e) override;
+
+protected:
+  //! Output a wrapped string to a stream.
+  //!
+  //!\param os Stream to which to output the wrapped string.
+  //!\param s String to wrap and output.
+  //!\param indentLevel Number of times to indent the output.
+  //!\param secondLineOffset Number of additional spaces with which to indent
+  //! all lines of the output except the first.
+  void spacePrint(std::ostream &os, const std::string &s, const int indentLevel,
+                  const int secondLineOffset) const;
+
+  //! Insert the usage command into a stream.
+  //!
+  //!\param [in] c Command in question.
+  //!\param [in, out] stream Stream into which to insert the usage command.
+  //!\param [out] secondLineOffset Offset determined by the program name.
+  virtual void _usage_command(TCLAP::CmdLineInterface &c,
+                              std::stringstream &stream,
+                              int &secondLineOffset) const;
+
+private:
+  //! Printing function object.
+  SpacePrinter spacePrinter;
+
+  //! Maximum width of wrapped output.
+  int maxWidth{75};
+
+  //! Number of spaces per indent level.
+  int indentSpaces{2};
+
+  //! Maximum additional indentation of output lines after the first.
+  int maxSecondLineOffset{24};
+};
+
+//! Output class for subcommands.
+class SubOutput : public BaseOutput {
+public:
+  virtual void version(TCLAP::CmdLineInterface &c) override;
+};
+
+//! Output class for supercommands.
+class SuperOutput : public BaseOutput {
+public:
+  virtual void version(TCLAP::CmdLineInterface &c) override;
+
+  virtual void usage(TCLAP::CmdLineInterface &c) override;
+
+protected:
+  virtual void _usage_command(TCLAP::CmdLineInterface &c,
+                              std::stringstream &stream,
+                              int &secondLineOffset) const override;
+};
+
+#endif
---
+++ src/output.cpp
@@ -0,0 +1,109 @@
+#include "output.hpp"
+
+#include <stdexcept>
+
+void SpacePrinter::operator()(std::ostream &os, const std::string &s,
+                              const int maxWidth, const int indentSpaces,
+                              const int secondLineOffset) const {
+  return TCLAP::StdOutput::spacePrint(os, s, maxWidth, indentSpaces,
+                                      secondLineOffset);
+}
+
+void BaseOutput::spacePrint(std::ostream &os, const std::string &s,
+                            const int indentLevel,
+                            const int secondLineOffset) const {
+  // Not worrying about extremely indented output or anything like that.
+  return spacePrinter(os, s, maxWidth, indentLevel * indentSpaces,
+                      std::min(secondLineOffset, maxSecondLineOffset));
+}
+
+void BaseOutput::usage(TCLAP::CmdLineInterface &c) {
+  int indentLevel;
+  int secondLineOffset;
+
+  std::stringstream stream;
+  stream << "usage: ";
+  _usage_command(c, stream, secondLineOffset);
+  indentLevel = 0;
+  spacePrint(std::cout, stream.str(), 0, secondLineOffset);
+
+  std::cout << std::endl;
+
+  indentLevel = 0;
+  secondLineOffset = 0;
+  spacePrint(std::cout, "arguments and options:", indentLevel,
+             secondLineOffset);
+
+  secondLineOffset = indentSpaces;
+  for (TCLAP::Arg const *const p : c.getArgList()) {
+    indentLevel = 1;
+    spacePrint(std::cout, p->longID(), indentLevel, secondLineOffset);
+    indentLevel = 2;
+    spacePrint(std::cout, p->getDescription(), indentLevel, secondLineOffset);
+  }
+}
+
+void BaseOutput::_usage_command(TCLAP::CmdLineInterface &c,
+                                std::stringstream &stream,
+                                int &secondLineOffset) const {
+  stream << c.getProgramName();
+  secondLineOffset =
+      std::min(static_cast<int>(stream.tellp()) + 1, maxSecondLineOffset);
+  for (TCLAP::Arg const *const p : c.getArgList()) {
+    stream << " " << p->shortID();
+  }
+}
+
+void BaseOutput::failure(TCLAP::CmdLineInterface &c, TCLAP::ArgException &e) {
+  const int indentLevel = 0;
+  int secondLineOffset;
+
+  std::stringstream stream;
+  stream << "parse error: ";
+  const int secondLineOffset = stream.tellp();
+  // Could use `e.ArgId()` too.
+  stream << e.error();
+  spacePrint(std::cerr, stream.str(), indentLevel, secondLineOffset);
+
+  std::cerr << std::endl;
+
+  secondLineOffset = indentSpaces;
+  stream.str("");
+  stream.clear();
+  stream << "Run '" << c.getProgramName() << " --help'"
+         << " for more information.";
+  spacePrint(std::cerr, stream.str(), indentLevel, secondLineOffset);
+  throw TCLAP::ExitException(1);
+}
+
+// `git commit --version` doesn't work.
+void SubOutput::version(TCLAP::CmdLineInterface &c) {
+  throw std::runtime_error("not implemented");
+}
+
+void SuperOutput::usage(TCLAP::CmdLineInterface &c) {
+  BaseOutput::usage(c);
+
+  std::cout << std::endl;
+
+  std::stringstream stream;
+  // `git --help` uses '<command>'.
+  stream << "Run '" << c.getProgramName() << " <command> --help'"
+         << " for more information.";
+  const int indentLevel = 0;
+  const int secondLineOffset = indentSpaces;
+  spacePrint(std::cout, stream.str(), indentLevel, secondLineOffset);
+}
+
+void SuperOutput::_usage_command(TCLAP::CmdLineInterface &c,
+                                 std::stringstream &stream,
+                                 int &secondLineOffset) const {
+  BaseOutput::_usage_command(c, stream, secondLineOffset);
+  // Mimicking the output of `git --help`.
+  stream << " "
+         << "[<args>]";
+}
+
+void SuperOutput::version(TCLAP::CmdLineInterface &c) {
+  std::cout << c.getProgramName() << " version " << c.getVersion() << std::endl;
+}
