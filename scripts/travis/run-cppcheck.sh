ecb8fa2a496724e6d02d7b9340230eb6aaeaf8b7
Ben Whitney <whitneybe@ornl.gov>
2021-08-03 15:22:32 -0400

    Delete Travis configuration.

--- scripts/travis/run-cppcheck.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CPPCHECK_BUILD_DIR=cppcheck-build-dir
-
-cd "${TRAVIS_BUILD_DIR}"
-mkdir "${CPPCHECK_BUILD_DIR}"
-cppcheck --project=build/compile_commands.json --cppcheck-build-dir="${CPPCHECK_BUILD_DIR}" --enable=all --quiet
-
-exit 0
