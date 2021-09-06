2909c48317ad4210fa3705c3f2dafe8640f2d276
Robert Underwood <rr.underwood94@gmail.com>
2021-09-06 15:20:54 -0400

    Various fixes for CMake and MGARD
    + enable CMAKE_EXPORT_COMPILE_COMMANDS to enable llvm based tooling
    + use idiomatic `include(CTest)` to enable/disable testing
    + use upstream provided detection modules; this will help when compiling
      under spack
    + allow asan and ubsan to be disabled on debug builds for situations
      where they cannot be used, but debug builds are still wanted
    + improved some checks that were incomplete and caused build failures
    + removed duplicate symbols that cause a linker failure when linking the
      CUDA version

--- src/cuda/ParallelHuffman/format.cc
+++ src/cuda/ParallelHuffman/format.cc
@@ -6,12 +6,6 @@
 
 using std::string;
 
-const string log_null = "       ";
-const string log_err = "\e[31m[ERR]\e[0m  ";
-const string log_dbg = "\e[34m[dbg]\e[0m  ";
-const string log_info = "\e[32m[info]\e[0m ";
-const string log_warn = "\e[31m[WARN]\e[0m ";
-
 // https://stackoverflow.com/a/26080768/8740097
 template <typename T> void huffman_gpu::log::build(std::ostream &o, T t) {
   o << t << std::endl;
