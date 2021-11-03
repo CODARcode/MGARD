90257ff5e701398f30cf1e6875d6f1764000e7f1
Ben Whitney <whitneybe@ornl.gov>
2021-11-03 10:32:08 -0400

    Add fallback TCLAP find module.

--- cmake/FindTCLAP.cmake
+++ cmake/FindTCLAP.cmake
@@ -11,17 +11,15 @@
 #
 #     TCLAP::TCLAP     - TCLAP library.
 
-set(TCLAP_INCLUDE_DIRS_PATHS ~/.local/include /usr/local/include /usr/include)
-
 set(TCLAP_INCLUDE_DIRS_ERROR_MESSAGE "TCLAP headers not found.")
 set(TCLAP_NOT_FOUND_MESSAGE "TCLAP not found.")
 
 set(TCLAP_FOUND TRUE)
 
 if(TCLAP_FIND_QUIETLY OR NOT TCLAP_FIND_REQUIRED)
-	find_path(TCLAP_INCLUDE_DIRS tclap PATHS ${TCLAP_INCLUDE_DIRS_PATHS})
+	find_path(TCLAP_INCLUDE_DIRS "tclap/Arg.h")
 else()
-	find_path(TCLAP_INCLUDE_DIRS tclap PATHS ${TCLAP_INCLUDE_DIRS_PATHS} REQUIRED)
+	find_path(TCLAP_INCLUDE_DIRS "tclap/Arg.h" REQUIRED)
 endif()
 
 if (TCLAP_INCLUDE_DIRS STREQUAL "TCLAP_INCLUDE_DIRS-NOTFOUND")
@@ -34,6 +32,9 @@ endif()
 if (TCLAP_FOUND)
 	add_library(TCLAP::TCLAP INTERFACE IMPORTED)
 	set_target_properties(TCLAP::TCLAP PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${TCLAP_INCLUDE_DIRS})
+	if(DEFINED TCLAP_FIND_VERSION)
+		message(STATUS "TCLAP found, but version number not checked (version ${TCLAP_FIND_VERSION} requested).")
+	endif()
 else()
 	if(TCLAP_FIND_REQUIRED)
 		message(FATAL_ERROR ${TCLAP_NOT_FOUND_MESSAGE})
