f42d5917c3a991cc3beba84a0c96f5da079474f1
Ben Whitney <whitneybe@ornl.gov>
2021-12-01 16:03:48 -0500

    Add header size, CRC32 serialization functions.

--- include/format.hpp
+++ include/format.hpp
@@ -3,7 +3,47 @@
 //!\file
 //!\brief Self-describing file format for compressed datasets.
 
-namespace mgard {} // namespace mgard
+#include <cstdint>
+
+#include <array>
+
+namespace mgard {
+
+// Size in bytes of the serialized header size.
+inline constexpr std::size_t HEADER_SIZE_SIZE = 8;
+
+// Size in bytes of the serialized header CRC32.
+inline constexpr std::size_t HEADER_CRC32_SIZE = 4;
+
+//! Deserialize header size.
+//!
+//!\param bytes Serialized header size.
+//!\return Size in bytes of the header.
+std::uint_least64_t deserialize_header_size(
+    const std::array<unsigned char, HEADER_SIZE_SIZE> &bytes);
+
+//! Deserialize header CRC32.
+//!
+//!\param bytes Serialized header CRC32.
+//!\return CRC32 of the header.
+std::uint_least32_t deserialize_header_crc32(
+    const std::array<unsigned char, HEADER_CRC32_SIZE> &bytes);
+
+//! Serialize header size.
+//!
+//!\param size Size in bytes of the header.
+//!\return Serialized header size.
+std::array<unsigned char, HEADER_SIZE_SIZE>
+serialize_header_size(std::uint_least64_t size);
+
+//! Serialize header CRC32.
+//!
+//!\param CRC32 of the header.
+//!\return bytes Serialized header CRC32.
+std::array<unsigned char, HEADER_CRC32_SIZE>
+serialize_header_crc32(std::uint_least64_t crc32);
+
+} // namespace mgard
 
 #include "format.tpp"
 #endif
--- include/format.tpp
+++ include/format.tpp
@@ -1,1 +1,43 @@
-namespace mgard {} // namespace mgard
+#include <cassert>
+#include <climits>
+
+#include <type_traits>
+
+namespace mgard {
+
+namespace {
+
+//! Deserialize an unsigned integer.
+template <typename T, std::size_t N>
+T deserialize(const std::array<unsigned char, N> &bytes) {
+  static_assert(std::is_unsigned<T>::value,
+                "can only deserialize unsigned integral types");
+  T out = 0;
+  // Multibyte metadata values are stored in big-endian format.
+  using It = typename std::array<unsigned char, N>::const_iterator;
+  for (It p = bytes.cbegin(); p != bytes.cend(); ++p) {
+    out <<= CHAR_BIT;
+    out ^= *p;
+  }
+  return out;
+}
+
+//! Serialize an unsigned integer.
+template <typename T, std::size_t N>
+std::array<unsigned char, N> serialize(T in) {
+  static_assert(std::is_unsigned<T>::value,
+                "can only serialize unsigned integral types");
+  std::array<unsigned char, N> bytes;
+  // Multibyte metadata values are stored in big-endian format.
+  using It = typename std::array<unsigned char, N>::reverse_iterator;
+  for (It p = bytes.rbegin(); p != bytes.rend(); ++p) {
+    *p = in;
+    in >>= CHAR_BIT;
+  }
+  assert(not in);
+  return bytes;
+}
+
+} // namespace
+
+} // namespace mgard
