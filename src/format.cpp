ea102e0844a6611755a31639ea835e9aedb28f9f
Ben Whitney <whitneybe@ornl.gov>
2021-12-01 15:59:25 -0500

    Remove file format helper functions.

--- src/format.cpp
+++ src/format.cpp
@@ -1,38 +1,3 @@
 #include "format.hpp"
 
-namespace mgard {
-
-std::array<uint8_t, 3> VersionNumber::to_array() {
-  return {a.major, a.minor, a.patch};
-}
-
-bool operator==(const VersionNumber &a, const VersionNumber &b) {
-  return operator==(a.to_array(), b.to_array());
-}
-
-bool operator!=(const VersionNumber &a, const VersionNumber &b) {
-  return operator!=(a.to_array(), b.to_array());
-}
-
-bool operator>=(const VersionNumber &a, const VersionNumber &b) {
-  return operator>=(a.to_array(), b.to_array());
-}
-
-bool operator>(const VersionNumber &a, const VersionNumber &b) {
-  return operator>(a.to_array(), b.to_array());
-}
-
-bool operator<=(const VersionNumber &a, const VersionNumber &b) {
-  return operator<=(a.to_array(), b.to_array());
-}
-
-bool operator<(const VersionNumber &a, const VersionNumber &b) {
-  return operator<(a.to_array(), b.to_array());
-}
-
-MetadataReader::MetadataReader(unsigned char const *const p) : p(p) {}
-
-MetadataWriter::MetadataWriter(std::vector<unsigned char> &buffer)
-    : buffer(buffer) {}
-
-} // namespace mgard
+namespace mgard {} // namespace mgard
