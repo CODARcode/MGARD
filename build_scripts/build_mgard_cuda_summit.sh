5745e9ee005f611eb19908c210ae34a907697adf
Jieyang Chen <cjy7117@gmail.com>
2022-02-27 23:11:54 -0500

    Update build scripts

--- build_scripts/build_mgard_cuda_summit.sh
+++ build_scripts/build_mgard_cuda_summit.sh
@@ -4,7 +4,7 @@
 # MGARD-X: MultiGrid Adaptive Reduction of Data Portable across GPUs and CPUs
 # Author: Jieyang Chen (chenj3@ornl.gov)
 # Date: April 2, 2021
-# Script for building MGARD-GPU
+# Script for building MGARD-X
 
 set -e
 set -x
@@ -13,72 +13,73 @@ module load cuda/11.4
 module load gcc/9
 module load cmake
 
-home_dir=$(pwd)
-external_dir=${home_dir}/external-cuda-summit
-mkdir -p ${external_dir}
+######## User Configurations ########
+# Source directory
+mgard_x_src_dir=.
+# Build directory
+build_dir=./build-cuda-summit
+# Number of processors used for building
+num_build_procs=8
+# Installtaion directory
+install_dir=./install-cuda-summit
+
 
 #build NVCOMP
-nvcomp_dir=${external_dir}/nvcomp
+nvcomp_dir=${build_dir}/nvcomp
 nvcomp_src_dir=${nvcomp_dir}/src
 nvcomp_build_dir=${nvcomp_dir}/build
-nvcomp_install_dir=${nvcomp_dir}/install
-nvcomp_library=${nvcomp_dir}/build/lib/libnvcomp.so
-if [ ! -d "${nvcomp_install_dir}" ]; then
-  rm -rf ${nvcomp_dir} && mkdir -p ${nvcomp_dir}
-  git clone https://github.com/NVIDIA/nvcomp.git ${nvcomp_src_dir}
-  cmake -S ${nvcomp_src_dir} -B ${nvcomp_build_dir}\
-      -DCMAKE_INSTALL_PREFIX=${nvcomp_install_dir}
-  cmake --build ${nvcomp_build_dir} -j8
-  cmake --install ${nvcomp_build_dir}
+nvcomp_install_dir=${install_dir}
+if [ ! -d "${nvcomp_src_dir}" ]; then
+  git clone -b v2.2.0 https://github.com/NVIDIA/nvcomp.git ${nvcomp_src_dir}
 fi
+mkdir -p ${nvcomp_build_dir}
+cmake -S ${nvcomp_src_dir} -B ${nvcomp_build_dir}\
+    -DCMAKE_INSTALL_PREFIX=${nvcomp_install_dir}
+cmake --build ${nvcomp_build_dir} -j ${num_build_procs}
+cmake --install ${nvcomp_build_dir}
 
 #build ZSTD
-zstd_dir=${external_dir}/zstd
+zstd_dir=${build_dir}/zstd
 zstd_src_dir=${zstd_dir}/src
 zstd_build_dir=${zstd_dir}/build
-zstd_install_dir=${zstd_dir}/install
-if [ ! -d "${zstd_install_dir}" ]; then
-  rm -rf ${zstd_dir} && mkdir -p ${zstd_dir}
+zstd_install_dir=${install_dir}
+if [ ! -d "${zstd_src_dir}" ]; then
   git clone -b v1.5.0 https://github.com/facebook/zstd.git ${zstd_src_dir}
-  cmake -S ${zstd_src_dir}/build/cmake -B ${zstd_build_dir}\
-      -DZSTD_MULTITHREAD_SUPPORT=ON\
-      -DCMAKE_INSTALL_LIBDIR=lib\
-      -DCMAKE_INSTALL_PREFIX=${zstd_install_dir}
-  cmake --build ${zstd_build_dir} -j8
-  cmake --install ${zstd_build_dir}
 fi
-
-export LD_LIBRARY_PATH=${zstd_install_dir}/lib:$LD_LIBRARY_PATH
+mkdir -p ${zstd_build_dir}
+cmake -S ${zstd_src_dir}/build/cmake -B ${zstd_build_dir}\
+    -DZSTD_MULTITHREAD_SUPPORT=ON\
+    -DCMAKE_INSTALL_LIBDIR=lib\
+    -DCMAKE_INSTALL_PREFIX=${zstd_install_dir}
+cmake --build ${zstd_build_dir} -j ${num_build_procs}
+cmake --install ${zstd_build_dir}
 
 #build Protobuf
-protobuf_dir=${external_dir}/protobuf
+protobuf_dir=${build_dir}/protobuf
 protobuf_src_dir=${protobuf_dir}/src
 protobuf_build_dir=${protobuf_dir}/build
-protobuf_install_dir=${protobuf_dir}/install
-if [ ! -d "${protobuf_install_dir}" ]; then
-  rm -rf ${protobuf_dir} && mkdir -p ${protobuf_dir}
-  git clone -b v3.19.4 https://github.com/protocolbuffers/protobuf.git ${protobuf_src_dir}
-  cd ${protobuf_src_dir} && git submodule update --init --recursive && cd ${home_dir}
-  cmake -S ${protobuf_src_dir}/cmake -B ${protobuf_build_dir}\
-      -Dprotobuf_BUILD_SHARED_LIBS=ON\
-      -DCMAKE_INSTALL_PREFIX=${protobuf_install_dir}
-  cmake --build ${protobuf_build_dir} -j8
-  cmake --install ${protobuf_build_dir}
+protobuf_install_dir=${install_dir}
+if [ ! -d "${protobuf_src_dir}" ]; then
+  git clone -b v3.19.4 --recurse-submodules https://github.com/protocolbuffers/protobuf.git ${protobuf_src_dir}
 fi
-
-
+mkdir -p ${protobuf_build_dir}
+cmake -S ${protobuf_src_dir}/cmake -B ${protobuf_build_dir}\
+    -Dprotobuf_BUILD_SHARED_LIBS=ON\
+    -DCMAKE_INSTALL_PREFIX=${protobuf_install_dir}
+cmake --build ${protobuf_build_dir} -j ${num_build_procs}
+cmake --install ${protobuf_build_dir}
 
 #build MGARD
-mgard_x_src_dir=${home_dir}
-mgard_x_build_dir=${home_dir}/build-cuda-summit
-mgard_x_install_dir=${home_dir}/install-cuda-summit
-rm -rf ${mgard_x_build_dir} && mkdir -p ${mgard_x_build_dir}
+mgard_x_build_dir=${build_dir}/mgard
+mgard_x_install_dir=${install_dir}
+mkdir -p ${mgard_x_build_dir}
 cmake -S ${mgard_x_src_dir} -B ${mgard_x_build_dir} \
     -DCMAKE_PREFIX_PATH="${nvcomp_install_dir};${zstd_install_dir}/lib/cmake/zstd;${protobuf_install_dir}"\
     -DMGARD_ENABLE_SERIAL=ON\
     -DMGARD_ENABLE_CUDA=ON\
     -DCMAKE_CUDA_ARCHITECTURES="70"\
+    -DMGARD_ENABLE_DOCS=OFF\
     -DCMAKE_BUILD_TYPE=Release\
     -DCMAKE_INSTALL_PREFIX=${mgard_x_install_dir}
-cmake --build ${mgard_x_build_dir} -j6
+cmake --build ${mgard_x_build_dir} -j ${num_build_procs}
 cmake --install ${mgard_x_build_dir}
--- build_scripts/build_mgard_serial.sh
+++ build_scripts/build_mgard_serial.sh
@@ -4,40 +4,61 @@
 # MGARD-X: MultiGrid Adaptive Reduction of Data Portable across GPUs and CPUs
 # Author: Jieyang Chen (chenj3@ornl.gov)
 # Date: April 2, 2021
-# Script for building MGARD-GPU
+# Script for building MGARD-X
 
 set -e
 set -x
 
-home_dir=$(pwd)
-external_dir=${home_dir}/external
-mkdir -p ${external_dir}
+######## User Configurations ########
+# Source directory
+mgard_x_src_dir=.
+# Build directory
+build_dir=./build-serial
+# Number of processors used for building
+num_build_procs=8
+# Installtaion directory
+install_dir=./install-serial
 
 #build ZSTD
-zstd_dir=${external_dir}/zstd
+zstd_dir=${build_dir}/zstd
 zstd_src_dir=${zstd_dir}/src
 zstd_build_dir=${zstd_dir}/build
-zstd_install_dir=${zstd_dir}/install
-if [ ! -d "${zstd_install_dir}" ]; then
-  rm -rf ${zstd_dir} && mkdir -p ${zstd_dir}
+zstd_install_dir=${install_dir}
+if [ ! -d "${zstd_src_dir}" ]; then
   git clone -b v1.5.0 https://github.com/facebook/zstd.git ${zstd_src_dir}
-  cmake -S ${zstd_src_dir}/build/cmake -B ${zstd_build_dir}\
-      -DZSTD_MULTITHREAD_SUPPORT=ON\
-      -DCMAKE_INSTALL_LIBDIR=lib\
-      -DCMAKE_INSTALL_PREFIX=${zstd_install_dir}
-  cmake --build ${zstd_build_dir} -j8
-  cmake --install ${zstd_build_dir}
 fi
+mkdir -p ${zstd_build_dir}
+cmake -S ${zstd_src_dir}/build/cmake -B ${zstd_build_dir}\
+    -DZSTD_MULTITHREAD_SUPPORT=ON\
+    -DCMAKE_INSTALL_LIBDIR=lib\
+    -DCMAKE_INSTALL_PREFIX=${zstd_install_dir}
+cmake --build ${zstd_build_dir} -j ${num_build_procs}
+cmake --install ${zstd_build_dir}
+
+#build Protobuf
+protobuf_dir=${build_dir}/protobuf
+protobuf_src_dir=${protobuf_dir}/src
+protobuf_build_dir=${protobuf_dir}/build
+protobuf_install_dir=${install_dir}
+if [ ! -d "${protobuf_src_dir}" ]; then
+  git clone -b v3.19.4 --recurse-submodules https://github.com/protocolbuffers/protobuf.git ${protobuf_src_dir}
+fi
+mkdir -p ${protobuf_build_dir}
+cmake -S ${protobuf_src_dir}/cmake -B ${protobuf_build_dir}\
+    -Dprotobuf_BUILD_SHARED_LIBS=ON\
+    -DCMAKE_INSTALL_PREFIX=${protobuf_install_dir}
+cmake --build ${protobuf_build_dir} -j ${num_build_procs}
+cmake --install ${protobuf_build_dir}
 
 #build MGARD
-mgard_x_src_dir=${home_dir}
-mgard_x_build_dir=${home_dir}/build
-mgard_x_install_dir=${home_dir}/install
-rm -rf ${mgard_x_build_dir} && mkdir -p ${mgard_x_build_dir}
+mgard_x_build_dir=${build_dir}/mgard
+mgard_x_install_dir=${install_dir}
+mkdir -p ${mgard_x_build_dir}
 cmake -S ${mgard_x_src_dir} -B ${mgard_x_build_dir} \
-    -DCMAKE_PREFIX_PATH="${zstd_install_dir}/lib/cmake/zstd"\
+    -DCMAKE_PREFIX_PATH="${nvcomp_install_dir};${zstd_install_dir}/lib/cmake/zstd;${protobuf_install_dir}"\
     -DMGARD_ENABLE_SERIAL=ON\
+    -DMGARD_ENABLE_DOCS=OFF\
     -DCMAKE_BUILD_TYPE=Release\
     -DCMAKE_INSTALL_PREFIX=${mgard_x_install_dir}
-cmake --build ${mgard_x_build_dir} -j8
+cmake --build ${mgard_x_build_dir} -j ${num_build_procs}
 cmake --install ${mgard_x_build_dir}
