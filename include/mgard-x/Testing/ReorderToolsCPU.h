61eabfed8d6b294d8d88aa309cc673b0abca668a
Jieyang Chen <cjy7117@gmail.com>
2022-03-17 12:05:42 -0400

    Update header of source files for release

--- include/mgard-x/Testing/ReorderToolsCPU.h
+++ include/mgard-x/Testing/ReorderToolsCPU.h
@@ -2,10 +2,10 @@
 #define REORDERTOOLSCPU_H
 
 /*
- * Copyright 2021, Oak Ridge National Laboratory.
+ * Copyright 2022, Oak Ridge National Laboratory.
  * MGARD-X: MultiGrid Adaptive Reduction of Data Portable across GPUs and CPUs
  * Author: Jieyang Chen (chenj3@ornl.gov)
- * Date: December 1, 2021
+ * Date: March 17, 2022
  */
 
 #include "TensorMeshHierarchy.hpp"
