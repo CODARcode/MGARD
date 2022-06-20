0e6c563d181c4a25d7c65f9436680bd85a97e8e6
Ben Whitney <whitneybe@ornl.gov>
2022-06-20 11:34:21 -0400

    Separate lossless compressor implementations.

---
+++ src/lossless_zlib.cpp
@@ -0,0 +1,88 @@
+#include "lossless.hpp"
+
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+namespace mgard {
+
+MemoryBuffer<unsigned char> compress_memory_z(void z_const *const src,
+                                              const std::size_t srcLen) {
+  const std::size_t BUFSIZE = 2048 * 1024;
+  std::vector<Bytef *> buffers;
+  std::vector<std::size_t> bufferLengths;
+
+  z_stream strm;
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.next_in = static_cast<Bytef z_const *>(src);
+  strm.avail_in = srcLen;
+  buffers.push_back(strm.next_out = new Bytef[BUFSIZE]);
+  bufferLengths.push_back(strm.avail_out = BUFSIZE);
+
+  deflateInit(&strm, Z_BEST_COMPRESSION);
+
+  while (strm.avail_in != 0) {
+    [[maybe_unused]] const int res = deflate(&strm, Z_NO_FLUSH);
+    assert(res == Z_OK);
+    if (strm.avail_out == 0) {
+      buffers.push_back(strm.next_out = new Bytef[BUFSIZE]);
+      bufferLengths.push_back(strm.avail_out = BUFSIZE);
+    }
+  }
+
+  int res = Z_OK;
+  while (res == Z_OK) {
+    if (strm.avail_out == 0) {
+      buffers.push_back(strm.next_out = new Bytef[BUFSIZE]);
+      bufferLengths.push_back(strm.avail_out = BUFSIZE);
+    }
+    res = deflate(&strm, Z_FINISH);
+  }
+
+  assert(res == Z_STREAM_END);
+  bufferLengths.back() -= strm.avail_out;
+  // Could just do `nbuffers * BUFSIZE - strm.avail_out`.
+  const std::size_t bufferLen =
+      std::accumulate(bufferLengths.begin(), bufferLengths.end(), 0);
+  unsigned char *const buffer = new unsigned char[bufferLen];
+  {
+    const std::size_t nbuffers = buffers.size();
+    unsigned char *p = buffer;
+    for (std::size_t i = 0; i < nbuffers; ++i) {
+      unsigned char const *const buffer = buffers.at(i);
+      const std::size_t bufferLength = bufferLengths.at(i);
+      std::copy(buffer, buffer + bufferLength, p);
+      p += bufferLength;
+      delete[] buffer;
+    }
+  }
+  deflateEnd(&strm);
+
+  return MemoryBuffer<unsigned char>(buffer, bufferLen);
+}
+
+void decompress_memory_z(void z_const *const src, const std::size_t srcLen,
+                         unsigned char *const dst, const std::size_t dstLen) {
+  z_stream strm = {};
+  strm.total_in = strm.avail_in = srcLen;
+  strm.total_out = strm.avail_out = dstLen;
+  strm.next_in = static_cast<Bytef z_const *>(src);
+  strm.next_out = reinterpret_cast<Bytef *>(dst);
+
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+
+  [[maybe_unused]] int res;
+  res = inflateInit2(&strm, (15 + 32)); // 15 window bits, and the +32 tells
+                                        // zlib to to detect if using gzip or
+                                        // zlib
+  assert(res == Z_OK);
+  res = inflate(&strm, Z_FINISH);
+  assert(res == Z_STREAM_END);
+  res = inflateEnd(&strm);
+  assert(res == Z_OK);
+}
+
+} // namespace mgard
---
+++ src/lossless_zstd.cpp
@@ -0,0 +1,60 @@
+#include "lossless.hpp"
+
+#include <cstdio>
+#include <cstdlib>
+
+#ifndef MGARD_ZSTD
+#error "This file requires ZSTD."
+#endif
+
+#include <zstd.h>
+
+namespace mgard {
+
+/*! CHECK
+ * Check that the condition holds. If it doesn't print a message and die.
+ */
+#define CHECK(cond, ...)                                                       \
+  do {                                                                         \
+    if (!(cond)) {                                                             \
+      std::fprintf(stderr, "%s:%d CHECK(%s) failed: ", __FILE__, __LINE__,     \
+                   #cond);                                                     \
+      std::fprintf(stderr, "" __VA_ARGS__);                                    \
+      std::fprintf(stderr, "\n");                                              \
+      std::exit(1);                                                            \
+    }                                                                          \
+  } while (0)
+
+/*! CHECK_ZSTD
+ * Check the zstd error code and die if an error occurred after printing a
+ * message.
+ */
+#define CHECK_ZSTD(fn, ...)                                                    \
+  do {                                                                         \
+    std::size_t const err = (fn);                                              \
+    CHECK(!ZSTD_isError(err), "%s", ZSTD_getErrorName(err));                   \
+  } while (0)
+
+MemoryBuffer<unsigned char> compress_memory_zstd(void const *const src,
+                                                 const std::size_t srcLen) {
+  const std::size_t cBuffSize = ZSTD_compressBound(srcLen);
+  unsigned char *const buffer = new unsigned char[cBuffSize];
+  const std::size_t cSize = ZSTD_compress(buffer, cBuffSize, src, srcLen, 1);
+  CHECK_ZSTD(cSize);
+  return MemoryBuffer<unsigned char>(buffer, cSize);
+}
+
+void decompress_memory_zstd(void const *const src, const std::size_t srcLen,
+                            unsigned char *const dst,
+                            const std::size_t dstLen) {
+  std::size_t const dSize = ZSTD_decompress(dst, dstLen, src, srcLen);
+  CHECK_ZSTD(dSize);
+
+  /* When zstd knows the content size, it will error if it doesn't match. */
+  CHECK(dstLen == dSize, "Impossible because zstd will check this condition!");
+}
+
+#undef CHECK_ZSTD
+#undef CHECK
+
+} // namespace mgard
