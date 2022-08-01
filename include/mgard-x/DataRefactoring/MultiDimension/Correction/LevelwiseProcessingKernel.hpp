71bf1d72388a33298c089e37ce5637f84f4030b9
Jieyang Chen <cjy7117@gmail.com>
2022-07-31 20:02:37 -0400

    Revise LWPK for the new dim/level order

--- include/mgard-x/DataRefactoring/MultiDimension/Correction/LevelwiseProcessingKernel.hpp
+++ include/mgard-x/DataRefactoring/MultiDimension/Correction/LevelwiseProcessingKernel.hpp
@@ -32,43 +32,57 @@ public:
                FunctorBase<DeviceType>::GetThreadIdX();
 
     SIZE idx[D];
-    SIZE firstD = div_roundup(v.getShape(0), F);
+    SIZE firstD = div_roundup(v.shape(D-1), F);
 
     SIZE bidx = FunctorBase<DeviceType>::GetBlockIdX();
-    idx[0] = (bidx % firstD) * F + FunctorBase<DeviceType>::GetThreadIdX();
+    // idx[0] = (bidx % firstD) * F + FunctorBase<DeviceType>::GetThreadIdX();
+    idx[D-1] = (bidx % firstD) * F + FunctorBase<DeviceType>::GetThreadIdX();
 
     // printf("firstD %d idx[0] %d\n", firstD, idx[0]);
 
     bidx /= firstD;
     if (D >= 2)
-      idx[1] = FunctorBase<DeviceType>::GetBlockIdY() *
+      idx[D-2] = FunctorBase<DeviceType>::GetBlockIdY() *
                    FunctorBase<DeviceType>::GetBlockDimY() +
                FunctorBase<DeviceType>::GetThreadIdY();
     if (D >= 3)
-      idx[2] = FunctorBase<DeviceType>::GetBlockIdZ() *
+      idx[D-3] = FunctorBase<DeviceType>::GetBlockIdZ() *
                    FunctorBase<DeviceType>::GetBlockDimZ() +
                FunctorBase<DeviceType>::GetThreadIdZ();
 
-    for (DIM d = 3; d < D; d++) {
-      idx[d] = bidx % v.getShape(d);
-      bidx /= v.getShape(d);
+    // for (DIM d = 3; d < D; d++) {
+    //   idx[d] = bidx % v.getShape(d);
+    //   bidx /= v.getShape(d);
+    // }
+    for (int d = D-4; d >= 0; d--) {
+      idx[d] = bidx % v.shape(d);
+      bidx /= v.shape(d);
     }
     // int z = blockIdx.z * blockDim.z + threadIdx.z;
     // int y = blockIdx.y * blockDim.y + threadIdx.y;
     // int x = blockIdx.z * blockDim.z + threadIdx.z;
     bool in_range = true;
+    // for (DIM d = 0; d < D; d++) {
+    //   if (idx[d] >= v.getShape(d))
+    //     in_range = false;
+    // }
+
     for (DIM d = 0; d < D; d++) {
-      if (idx[d] >= v.getShape(d))
+      if (idx[d] >= v.shape(d))
         in_range = false;
     }
     if (in_range) {
       // printf("%d %d %d %d\n", idx[3], idx[2], idx[1], idx[0]);
-      if (OP == COPY)
-        *work(idx) = *v(idx);
-      if (OP == ADD)
-        *work(idx) += *v(idx);
-      if (OP == SUBTRACT)
-        *work(idx) -= *v(idx);
+      if (OP == COPY) {
+        // *work(idx) = *v(idx);
+        work[idx] = v[idx];
+      }
+      if (OP == ADD) {
+        work[idx] += v[idx];
+      }
+      if (OP == SUBTRACT) {
+        work[idx] -= v[idx];
+      }
     }
   }
 
@@ -99,10 +113,10 @@ public:
     SIZE total_thread_y = 1;
     SIZE total_thread_x = 1;
     if (D >= 3)
-      total_thread_z = v.getShape(2);
+      total_thread_z = v.shape(D-3);
     if (D >= 2)
-      total_thread_y = v.getShape(1);
-    total_thread_x = v.getShape(0);
+      total_thread_y = v.shape(D-2);
+    total_thread_x = v.shape(D-1);
 
     SIZE tbx, tby, tbz, gridx, gridy, gridz;
     size_t sm_size = functor.shared_memory_size();
@@ -112,8 +126,8 @@ public:
     gridz = ceil((float)total_thread_z / tbz);
     gridy = ceil((float)total_thread_y / tby);
     gridx = ceil((float)total_thread_x / tbx);
-    for (DIM d = 3; d < D; d++) {
-      gridx *= v.getShape(d);
+    for (int d = D-4; d >= 0; d--) {
+      gridx *= v.shape(d);
     }
     // printf("%u %u %u\n", shape.dataHost()[2], shape.dataHost()[1],
     // shape.dataHost()[0]); PrintSubarray("shape", shape);
@@ -125,7 +139,7 @@ public:
   void Execute(SubArray<D, T, DeviceType> v, SubArray<D, T, DeviceType> work,
                int queue_idx) {
 
-    int range_l = std::min(6, (int)std::log2(v.getShape(0)) - 1);
+    int range_l = std::min(6, (int)std::log2(v.shape(D-1)) - 1);
     int prec = TypeToIdx<T>();
     int config = AutoTuner<DeviceType>::autoTuningTable.lwpk[prec][range_l];
 
