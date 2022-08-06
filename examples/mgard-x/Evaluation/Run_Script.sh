42097669c1235586c1b4ce19690652b5dd7da1c3
Jieyang Chen <cjy7117@gmail.com>
2022-08-05 21:49:43 -0400

    Update evaluation run script

--- examples/mgard-x/Evaluation/Run_Script.sh
+++ examples/mgard-x/Evaluation/Run_Script.sh
@@ -12,20 +12,20 @@ E3SM_DATA=$DATA_DIR/temperature.dat
 # $MgardSerialExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5.5e-1 -s inf -v # actual error 1e-2
 # $MgardSerialExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e0 -s inf -v # actual error 1e-1
 
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-3 -s inf -l 0 -v -d $1
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-2 -s inf -l 0 -v -d $1
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5.5e-1 -s inf -l 0 -v -d $1
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e0 -s inf -l 0 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-3 -s inf -l 0 -r 0 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-2 -s inf -l 0 -r 0 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5.5e-1 -s inf -l 0 -r 0 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e0 -s inf -l 0 -r 0 -v -d $1
 
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-3 -s inf -l 1 -v -d $1
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-2 -s inf -l 1 -v -d $1
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5.5e-1 -s inf -l 1 -v -d $1
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e0 -s inf -l 1 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-3 -s inf -l 1 -r 0 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-2 -s inf -l 1 -r 0 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5.5e-1 -s inf -l 1 -r 0 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e0 -s inf -l 1 -r 0 -v -d $1
 
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-3 -s inf -l 2 -v -d $1
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-2 -s inf -l 2 -v -d $1
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5.5e-1 -s inf -l 2 -v -d $1
-# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e0 -s inf -l 2 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-3 -s inf -l 2 -r 0 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e-2 -s inf -l 2 -r 0 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5.5e-1 -s inf -l 2 -r 0 -v -d $1
+# $MgardXExec -z -i $NYX_DATA -c out.mgard -t s -n 3 512 512 512 -m rel -e 5e0 -s inf -l 2 -r 0 -v -d $1
 
 
 
@@ -43,20 +43,20 @@ e1=1.7e-3
 e2=1.8e-2
 e3=1.7e-1
 e4=4e0
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e1 -s inf -l 0 -v -d $1
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e2 -s inf -l 0 -v -d $1
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e3 -s inf -l 0 -v -d $1
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e4 -s inf -l 0 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e1 -s inf -l 0 -r 0 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e2 -s inf -l 0 -r 0 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e3 -s inf -l 0 -r 0 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e4 -s inf -l 0 -r 0 -v -d $1
 
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e1 -s inf -l 1 -v -d $1
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e2 -s inf -l 1 -v -d $1
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e3 -s inf -l 1 -v -d $1
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e4 -s inf -l 1 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e1 -s inf -l 1 -r 0 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e2 -s inf -l 1 -r 0 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e3 -s inf -l 1 -r 0 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e4 -s inf -l 1 -r 0 -v -d $1
 
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e1 -s inf -l 2 -v -d $1
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e2 -s inf -l 2 -v -d $1
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e3 -s inf -l 2 -v -d $1
-# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e4 -s inf -l 2 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e1 -s inf -l 2 -r 0 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e2 -s inf -l 2 -r 0 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e3 -s inf -l 2 -r 0 -v -d $1
+# $MgardXExec -z -i $XGC_DATA -c out.mgard -t d -n 3 312 16395 39 -m rel -e $e4 -s inf -l 2 -r 0 -v -d $1
 
 
 
@@ -75,17 +75,17 @@ e2=1e-2
 e3=1.041e-1
 e3=2.041e-1
 e4=4.05e0
-# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e1 -s inf -l 0 -v -d $1
-# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e2 -s inf -l 0 -v -d $1
-# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e3 -s inf -l 0 -v -d $1
-# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e4 -s inf -l 0 -v -d $1
+# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e1 -s inf -l 0 -r 0 -v -d $1
+# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e2 -s inf -l 0 -r 0 -v -d $1
+# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e3 -s inf -l 0 -r 0 -v -d $1
+# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e4 -s inf -l 0 -r 0 -v -d $1
 
-# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e1 -s inf -l 1 -v -d $1
-# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e2 -s inf -l 1 -v -d $1
-# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e3 -s inf -l 1 -v -d $1
-# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e4 -s inf -l 1 -v -d $1
+# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e1 -s inf -l 1 -r 0 -v -d $1
+# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e2 -s inf -l 1 -r 0 -v -d $1
+# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e3 -s inf -l 1 -r 0 -v -d $1
+# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e4 -s inf -l 1 -r 0 -v -d $1
 
 # $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e1 -s inf -l 2 -v -d $1
 # $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e2 -s inf -l 2 -v -d $1
 # $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e3 -s inf -l 2 -v -d $1
-$MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e4 -s inf -l 3 -v -d $1
+# $MgardXExec -z -i $E3SM_DATA -c out.mgard -t s -n 3 72 1444 359 -m rel -e $e4 -s inf -l 3 -r 0 -v -d $1
