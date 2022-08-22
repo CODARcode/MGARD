dcf65a201b92bd7921f4ba0e12432b600df1af7c
Jieyang Chen <cjy7117@gmail.com>
2022-08-22 15:17:22 -0400

    Adding logging functions

--- include/mgard-x/RuntimeX/Utilities/Message.h
+++ include/mgard-x/RuntimeX/Utilities/Message.h
@@ -10,12 +10,24 @@ using std::string;
 namespace mgard_x {
 namespace log {
 
+extern bool enable_log_info;
+extern bool enable_log_time;
+extern bool enable_log_dbg;
+extern bool enable_log_warn;
+extern bool enable_log_err;
+
 extern const string log_null;
-extern const string log_err;
-extern const string log_dbg;
 extern const string log_info;
-extern const string log_warn;
 extern const string log_time;
+extern const string log_dbg;
+extern const string log_warn;
+extern const string log_err;
+
+void info(std::string msg);
+void time(std::string msg);
+void dbg(std::string msg);
+void warn(std::string msg);
+void err(std::string msg);
 
 // https://stackoverflow.com/a/26080768/8740097
 template <typename T> void build(std::ostream &o, T t);
