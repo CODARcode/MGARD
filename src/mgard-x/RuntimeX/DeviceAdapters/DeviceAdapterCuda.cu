9b1ee08f0a1ffdfea087742bace2f8c3ed3a8a28
Jieyang Chen <cjy7117@gmail.com>
2022-09-19 11:20:46 -0400

    Simplify domain decomposition function

--- src/mgard-x/RuntimeX/DeviceAdapters/DeviceAdapterCuda.cu
+++ src/mgard-x/RuntimeX/DeviceAdapters/DeviceAdapterCuda.cu
@@ -14,7 +14,7 @@ DeviceQueues<CUDA> DeviceRuntime<CUDA>::queues;
 DeviceSpecification<CUDA> DeviceRuntime<CUDA>::DeviceSpecs;
 
 bool DeviceRuntime<CUDA>::SyncAllKernelsAndCheckErrors = false;
-bool MemoryManager<CUDA>::ReduceMemoryFootprint = true;
+bool MemoryManager<CUDA>::ReduceMemoryFootprint = false;
 bool DeviceRuntime<CUDA>::TimingAllKernels = false;
 bool DeviceRuntime<CUDA>::PrintKernelConfig = false;
 
