c9febc6724dc81321058f5806057aecf2b5d52df
Jieyang Chen <cjy7117@gmail.com>
2022-09-30 11:57:19 -0400

    Update examples

---
+++ examples/mgard-x/LowLevelAPIs/SYCL/Example.cpp
@@ -0,0 +1,43 @@
+#include "mgard/compress_x_lowlevel.hpp"
+
+#include <iostream>
+#include <vector>
+int main() {
+
+  mgard_x::SIZE n1 = 10;
+  mgard_x::SIZE n2 = 20;
+  mgard_x::SIZE n3 = 30;
+
+  // prepare
+  std::cout << "Preparing data...";
+  double *in_array_cpu = new double[n1 * n2 * n3];
+  //... load data into in_array_cpu
+  std::vector<mgard_x::SIZE> shape{n1, n2, n3};
+  mgard_x::Config config;
+  config.lossless = mgard_x::lossless_type::Huffman;
+  mgard_x::Hierarchy<3, double, mgard_x::SYCL> hierarchy(shape, config);
+  mgard_x::Array<3, double, mgard_x::SYCL> in_array(shape);
+  in_array.load(in_array_cpu);
+  std::cout << "Done\n";
+
+  std::cout << "Compressing with MGARD-X SYCL backend...";
+  double tol = 0.01, s = 0, norm;
+  mgard_x::CompressionLowLevelWorkspace workspace(hierarchy);
+  mgard_x::Array<1, unsigned char, mgard_x::SYCL> compressed_array;
+  mgard_x::compress(hierarchy, in_array, mgard_x::error_bound_type::REL, tol, s,
+                    norm, config, workspace, compressed_array);
+  // Get compressed size in number of bytes.
+  size_t compressed_size = compressed_array.shape(0);
+  unsigned char *compressed_array_cpu = compressed_array.hostCopy();
+  std::cout << "Done\n";
+
+  std::cout << "Decompressing with MGARD-X SYCL backend...";
+  // decompression
+  mgard_x::Array<3, double, mgard_x::SYCL> decompressed_array;
+  mgard_x::decompress(hierarchy, compressed_array,
+                      mgard_x::error_bound_type::REL, tol, s, norm, config,
+                      workspace, decompressed_array);
+  delete[] in_array_cpu;
+  double *decompressed_array_cpu = decompressed_array.hostCopy();
+  std::cout << "Done\n";
+}
