1998ad72c656bfb94acaff7f4753e4148be43564
Jieyang Chen <cjy7117@gmail.com>
2022-09-30 11:55:56 -0400

    Add auto tuner for OPENMP backend

--- src/mgard-x/RuntimeX/AutoTuners/AutoTunerOpenmp.cpp
+++ src/mgard-x/RuntimeX/AutoTuners/AutoTunerOpenmp.cpp
@@ -50,7 +50,7 @@ int AutoTuningTable<OPENMP>::ipk2_nd[2][9] = {{0, 1, 2, 2, 0, 0, 0, 0, 0},
 int AutoTuningTable<OPENMP>::ipk3_nd[2][9] = {{0, 2, 3, 2, 0, 0, 0, 0, 0},
                                               {0, 3, 4, 2, 0, 0, 0, 0, 0}};
 
-int AutoTuningTable<OPENMP>::lwpk[2][9] = {{5, 2, 2, 1, 0, 2, 1, 0, 0},
+int AutoTuningTable<OPENMP>::lwpk[2][9] = {{5, 2, 2, 1, 0, 2, 0, 0, 0},
                                            {2, 2, 3, 3, 0, 2, 5, 0, 0}};
 
 int AutoTuningTable<OPENMP>::lwqzk[2][9] = {{5, 2, 2, 1, 0, 2, 1, 0, 0},
