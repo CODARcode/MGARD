297532ac848d98c371f14ec105dea3a8c82deab9
Jieyang Chen <cjy7117@gmail.com>
2022-10-06 21:58:04 -0400

    Make LPK_ND and IPK_ND use the new DeviceLauncher

--- include/mgard-x/DataRefactoring/SingleDimension/Correction/CalcCorrection.hpp
+++ include/mgard-x/DataRefactoring/SingleDimension/Correction/CalcCorrection.hpp
@@ -38,10 +38,12 @@ void CalcCorrection(Hierarchy<D, T, DeviceType> &hierarchy,
   if (curr_dim == D - 1) {
     curr_dim_r = D - 3, curr_dim_c = D - 2, curr_dim_f = D - 1;
     correction.project(curr_dim_r, curr_dim_c, curr_dim_f);
-    Ipk1Reo<D, T, DeviceType>().Execute(curr_dim_r, curr_dim_c, curr_dim_f,
+    DeviceLauncher<DeviceType>::Execute(
+        Ipk1ReoKernel<D, T, DeviceType>(curr_dim_r, curr_dim_c, curr_dim_f,
                                         SubArray(hierarchy.am(l - 1, D - 1)),
                                         SubArray(hierarchy.bm(l - 1, D - 1)),
-                                        correction, queue_idx);
+                                        correction),
+        queue_idx);
     if (singledim_refactoring_debug_print) {
       PrintSubarray("Ipk1Reo", correction);
     }
@@ -49,30 +51,36 @@ void CalcCorrection(Hierarchy<D, T, DeviceType> &hierarchy,
   } else if (curr_dim == D - 2) {
     curr_dim_r = D - 3, curr_dim_c = D - 2, curr_dim_f = D - 1;
     correction.project(curr_dim_r, curr_dim_c, curr_dim_f);
-    Ipk2Reo<D, T, DeviceType>().Execute(curr_dim_r, curr_dim_c, curr_dim_f,
+    DeviceLauncher<DeviceType>::Execute(
+        Ipk2ReoKernel<D, T, DeviceType>(curr_dim_r, curr_dim_c, curr_dim_f,
                                         SubArray(hierarchy.am(l - 1, D - 2)),
                                         SubArray(hierarchy.bm(l - 1, D - 2)),
-                                        correction, queue_idx);
+                                        correction),
+        queue_idx);
     if (singledim_refactoring_debug_print) {
       PrintSubarray("Ipk2Reo", correction);
     }
   } else if (curr_dim == D - 3) {
     curr_dim_r = D - 3, curr_dim_c = D - 2, curr_dim_f = D - 1;
     correction.project(curr_dim_r, curr_dim_c, curr_dim_f);
-    Ipk3Reo<D, T, DeviceType>().Execute(curr_dim_r, curr_dim_c, curr_dim_f,
+    DeviceLauncher<DeviceType>::Execute(
+        Ipk3ReoKernel<D, T, DeviceType>(curr_dim_r, curr_dim_c, curr_dim_f,
                                         SubArray(hierarchy.am(l - 1, D - 3)),
                                         SubArray(hierarchy.bm(l - 1, D - 3)),
-                                        correction, queue_idx);
+                                        correction),
+        queue_idx);
     if (singledim_refactoring_debug_print) {
       PrintSubarray("Ipk3Reo", correction);
     }
   } else {
     curr_dim_r = curr_dim, curr_dim_c = D - 2, curr_dim_f = D - 1;
     correction.project(curr_dim_r, curr_dim_c, curr_dim_f);
-    Ipk3Reo<D, T, DeviceType>().Execute(curr_dim_r, curr_dim_c, curr_dim_f,
+    DeviceLauncher<DeviceType>::Execute(
+        Ipk3ReoKernel<D, T, DeviceType>(curr_dim_r, curr_dim_c, curr_dim_f,
                                         SubArray(hierarchy.am(l - 1, curr_dim)),
                                         SubArray(hierarchy.bm(l - 1, curr_dim)),
-                                        correction, queue_idx);
+                                        correction),
+        queue_idx);
     if (singledim_refactoring_debug_print) {
       PrintSubarray("Ipk3Reo", correction);
     }
