c59a3a14ee0f534b969323f03d546fc26e7d9ca3
Jieyang Chen <cjy7117@gmail.com>
2022-10-06 20:22:58 -0400

    Make LWPK use the new DeviceLauncher

--- include/mgard-x/DataRefactoring/MultiDimension/CopyND/CopyND.hpp
+++ include/mgard-x/DataRefactoring/MultiDimension/CopyND/CopyND.hpp
@@ -21,7 +21,8 @@ template <DIM D, typename T, typename DeviceType>
 void CopyND(SubArray<D, T, DeviceType> dinput,
             SubArray<D, T, DeviceType> &doutput, int queue_idx) {
 
-  LwpkReo<D, T, COPY, DeviceType>().Execute(dinput, doutput, queue_idx);
+  DeviceLauncher<DeviceType>::Execute(
+      LwpkReoKernel<D, T, COPY, DeviceType>(dinput, doutput), queue_idx);
 }
 
 } // namespace mgard_x
