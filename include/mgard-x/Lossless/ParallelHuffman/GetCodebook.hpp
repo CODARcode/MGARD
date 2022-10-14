4a12f0ff80603196357506888f130d0cb39f6593
Jieyang Chen <cjy7117@gmail.com>
2022-10-14 16:41:25 -0400

    Apply Huffman workspace

--- include/mgard-x/Lossless/ParallelHuffman/GetCodebook.hpp
+++ include/mgard-x/Lossless/ParallelHuffman/GetCodebook.hpp
@@ -22,7 +22,7 @@ void GetCodebook(int dict_size,
                  SubArray<1, unsigned int, DeviceType> _d_freq_subarray,
                  SubArray<1, H, DeviceType> _d_codebook_subarray,
                  SubArray<1, uint8_t, DeviceType> _d_decode_meta_subarray,
-                 SubArray<1, int, DeviceType> status_subarray) {
+                 HuffmanWorkspace<Q, H, DeviceType> &workspace) {
   // Metadata
   auto type_bw = sizeof(H) * 8;
 
@@ -36,26 +36,20 @@ void GetCodebook(int dict_size,
   // Sort Qcodes by frequency
   DeviceLauncher<DeviceType>::Execute(
       FillArraySequenceKernel(_d_qcode_subarray), 0);
+
   DeviceCollective<DeviceType>::SortByKey(dict_size, _d_freq_subarray,
                                           _d_qcode_subarray, 0);
 
-  unsigned int *d_first_nonzero_index;
-  unsigned int first_nonzero_index;
-  Array<1, unsigned int, DeviceType> first_nonzero_index_array({1});
-  first_nonzero_index_array.memset(0xff);
-  // Avoid H2D copy
-  // first_nonzero_index_array.load((unsigned int *)&dict_size);
-
-  SubArray<1, unsigned int, DeviceType> d_first_nonzero_index_subarray(
-      {1}, d_first_nonzero_index);
-
   DeviceLauncher<DeviceType>::Execute(
       GetFirstNonzeroIndexKernel<unsigned int, DeviceType>(
-          _d_freq_subarray, first_nonzero_index_array),
+          _d_freq_subarray, workspace.first_nonzero_index_subarray),
       0);
 
+  unsigned int first_nonzero_index;
+  MemoryManager<DeviceType>().Copy1D(
+      &first_nonzero_index, workspace.first_nonzero_index_subarray(IDX(0)), 1,
+      0);
   DeviceRuntime<DeviceType>::SyncQueue(0);
-  first_nonzero_index = first_nonzero_index_array.hostCopy()[0];
 
   if (debug_print_huffman) {
     PrintSubarray("SortByKey::_d_freq_subarray", _d_freq_subarray);
@@ -64,65 +58,31 @@ void GetCodebook(int dict_size,
   }
 
   int nz_dict_size = dict_size - first_nonzero_index;
-  // unsigned int *_nz_d_freq = _d_freq + first_nonzero_index;
-  // H *_nz_d_codebook = _d_codebook + first_nonzero_index;
-  int nz_nblocks = (nz_dict_size / 1024) + 1;
 
   SubArray<1, unsigned int, DeviceType> _nz_d_freq_subarray(
       {(SIZE)nz_dict_size}, _d_freq_subarray(first_nonzero_index));
   SubArray<1, H, DeviceType> _nz_d_codebook_subarray(
       {(SIZE)nz_dict_size}, _d_codebook_subarray(first_nonzero_index));
 
-  Array<1, unsigned int, DeviceType> CL_array({(SIZE)nz_dict_size});
-  Array<1, int, DeviceType> lNodesLeader_array({(SIZE)nz_dict_size});
-  Array<1, unsigned int, DeviceType> iNodesFreq_array({(SIZE)nz_dict_size});
-  Array<1, int, DeviceType> iNodesLeader_array({(SIZE)nz_dict_size});
-  Array<1, unsigned int, DeviceType> tempFreq_array({(SIZE)nz_dict_size});
-  Array<1, int, DeviceType> tempIsLeaf_array({(SIZE)nz_dict_size});
-  Array<1, int, DeviceType> tempIndex_array({(SIZE)nz_dict_size});
-  Array<1, unsigned int, DeviceType> copyFreq_array({(SIZE)nz_dict_size});
-  Array<1, int, DeviceType> copyIsLeaf_array({(SIZE)nz_dict_size});
-  Array<1, int, DeviceType> copyIndex_array({(SIZE)nz_dict_size});
-
-  CL_array.memset(0);
-
-  SIZE mblocks = (DeviceRuntime<DeviceType>::GetMaxNumThreadsPerTB() /
-                  DeviceRuntime<DeviceType>::GetWarpSize()) *
-                 DeviceRuntime<DeviceType>::GetNumSMs();
-  uint32_t *diagonal_path_intersections;
-  Array<1, uint32_t, DeviceType> diagonal_path_intersections_array(
-      {2 * (mblocks + 1)});
-
-  DeviceRuntime<DeviceType>::SyncDevice();
-
-  SubArray<1, unsigned int, DeviceType> CL_subarray(CL_array);
-  SubArray<1, int, DeviceType> lNodesLeader_subarray(lNodesLeader_array);
-  SubArray<1, unsigned int, DeviceType> iNodesFreq_subarray(iNodesFreq_array);
-  SubArray<1, int, DeviceType> iNodesLeader_subarray(iNodesLeader_array);
-  SubArray<1, unsigned int, DeviceType> tempFreq_subarray(tempFreq_array);
-  SubArray<1, int, DeviceType> tempIsLeaf_subarray(tempIsLeaf_array);
-  SubArray<1, int, DeviceType> tempIndex_subarray(tempIndex_array);
-  SubArray<1, unsigned int, DeviceType> copyFreq_subarray(copyFreq_array);
-  SubArray<1, int, DeviceType> copyIsLeaf_subarray(copyIsLeaf_array);
-  SubArray<1, int, DeviceType> copyIndex_subarray(copyIndex_array);
-  SubArray<1, uint32_t, DeviceType> diagonal_path_intersections_subarray(
-      diagonal_path_intersections_array);
-
   DeviceLauncher<DeviceType>::Execute(
       GenerateCLKernel<unsigned int, DeviceType>(
-          _nz_d_freq_subarray, CL_subarray, nz_dict_size, _nz_d_freq_subarray,
-          lNodesLeader_subarray, iNodesFreq_subarray, iNodesLeader_subarray,
-          tempFreq_subarray, tempIsLeaf_subarray, tempIndex_subarray,
-          copyFreq_subarray, copyIsLeaf_subarray, copyIndex_subarray,
-          diagonal_path_intersections_subarray, status_subarray),
+          _nz_d_freq_subarray, workspace.CL_subarray, nz_dict_size,
+          _nz_d_freq_subarray, workspace.lNodesLeader_subarray,
+          workspace.iNodesFreq_subarray, workspace.iNodesLeader_subarray,
+          workspace.tempFreq_subarray, workspace.tempIsLeaf_subarray,
+          workspace.tempIndex_subarray, workspace.copyFreq_subarray,
+          workspace.copyIsLeaf_subarray, workspace.copyIndex_subarray,
+          workspace.diagonal_path_intersections_subarray,
+          workspace.status_subarray),
       0);
 
   unsigned int max_CL;
-  MemoryManager<DeviceType>().Copy1D(&max_CL, CL_subarray(IDX(0)), 1, 0);
+  MemoryManager<DeviceType>().Copy1D(&max_CL, workspace.CL_subarray(IDX(0)), 1,
+                                     0);
   DeviceRuntime<DeviceType>::SyncQueue(0);
 
   if (debug_print_huffman) {
-    PrintSubarray("GenerateCL::CL_subarray", CL_subarray);
+    PrintSubarray("GenerateCL::CL_subarray", workspace.CL_subarray);
     std::cout << "GenerateCL: max_CL" << max_CL << std::endl;
   }
 
@@ -139,23 +99,23 @@ void GetCodebook(int dict_size,
 
   DeviceLauncher<DeviceType>::Execute(
       GenerateCWKernel<unsigned int, H, DeviceType>(
-          CL_subarray, _nz_d_codebook_subarray, _d_first_subarray,
-          _d_entry_subarray, nz_dict_size, status_subarray),
+          workspace.CL_subarray, _nz_d_codebook_subarray, _d_first_subarray,
+          _d_entry_subarray, nz_dict_size, workspace.status_subarray),
       0);
 
   DeviceLauncher<DeviceType>::Execute(
       ReverseArrayKernel<H, DeviceType>(_d_codebook_subarray), 0);
   DeviceLauncher<DeviceType>::Execute(
       ReverseArrayKernel<Q, DeviceType>(_d_qcode_subarray), 0);
-  DeviceRuntime<DeviceType>::SyncQueue(0);
 
-  Array<1, H, DeviceType> _d_codebook_array_org(
-      {_d_codebook_subarray.shape(0)});
-  _d_codebook_array_org.load(_d_codebook_subarray.data());
-  SubArray _d_codebook_subarray_org(_d_codebook_array_org);
+  MemoryManager<DeviceType>().Copy1D(workspace._d_codebook_subarray_org.data(),
+                                     _d_codebook_subarray.data(),
+                                     _d_codebook_subarray.shape(0), 0);
+
   DeviceLauncher<DeviceType>::Execute(
-      ReorderByIndexKernel<H, Q, DeviceType>(
-          _d_codebook_subarray_org, _d_codebook_subarray, _d_qcode_subarray),
+      ReorderByIndexKernel<H, Q, DeviceType>(workspace._d_codebook_subarray_org,
+                                             _d_codebook_subarray,
+                                             _d_qcode_subarray),
       0);
   DeviceRuntime<DeviceType>::SyncQueue(0);
 }
