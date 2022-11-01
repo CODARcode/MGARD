6d4bd7b58c0377b54c1cf8d37208d223b98b8895
Jieyang Chen <cjy7117@gmail.com>
2022-11-01 11:22:48 -0400

    Add MDRMetaData

--- include/mgard-x/MDR-X/Refactor/RefactorInterface.hpp
+++ include/mgard-x/MDR-X/Refactor/RefactorInterface.hpp
@@ -32,10 +32,11 @@ class RefactorInterface {
 public:
   virtual ~RefactorInterface() = default;
 
-  virtual void refactor(Array<D, T_data, DeviceType> &data_array, uint8_t num_bitplanes, 
+  virtual void refactor(Array<D, T_data, DeviceType> &data_array, uint8_t num_bitplanes,
+                        ::mgard_x::MDR::MDRMetaData<D, T_data, DeviceType> &mdr_metadata,
                         ::mgard_x::MDR::MDRData<D, T_data, DeviceType> &mdr_data, int queue_idx) = 0;
 
-  virtual void write_metadata(::mgard_x::MDR::MDRData<D, T_data, DeviceType> &mdr_data) = 0;
+  virtual void write_metadata(::mgard_x::MDR::MDRMetaData<D, T_data, DeviceType> &mdr_metadata) = 0;
 
   virtual void print() const = 0;
 };
