b6ec6799b6676776052c0f1d1d667c0176bdf2e2
Jieyang Chen <cjy7117@gmail.com>
2022-11-06 22:43:52 -0500

    Add serilization for MDR metadata

--- include/mgard-x/RuntimeX/DataStructures/MDRMetadata.hpp
+++ include/mgard-x/RuntimeX/DataStructures/MDRMetadata.hpp
@@ -14,7 +14,21 @@ namespace MDR {
 class MDRMetadata {
 public:
   MDRMetadata(): num_levels(0), num_bitplanes(0) {}
-  MDRMetadata(SIZE num_levels, SIZE num_bitplanes): num_levels(num_levels), num_bitplanes(num_bitplanes) {}
+
+  void Initialize(SIZE num_levels, SIZE num_bitplanes) {
+    this->num_levels = num_levels;
+    this->num_bitplanes = num_bitplanes;
+    level_error_bounds.resize(num_levels);
+    level_squared_errors.resize(num_levels);
+    level_sizes.resize(num_levels);
+    for (int i = 0; i < num_levels; i++) {
+      level_squared_errors[i].resize(num_bitplanes+1);
+      level_sizes[i].resize(num_bitplanes);
+    }
+  }
+  MDRMetadata(SIZE num_levels, SIZE num_bitplanes): num_levels(num_levels), num_bitplanes(num_bitplanes) {
+    Initialize(num_levels, num_bitplanes);
+  }
 
   using T_error = double;
   // Metadata
@@ -54,6 +68,55 @@ public:
       prev_used_level_num_bitplanes[level_idx] = loaded_level_num_bitplanes[level_idx];
     }
   }
+
+  SIZE MetadataSize() {
+    SIZE metadata_size = 0;
+    metadata_size += sizeof(SIZE) * 2;
+    metadata_size += sizeof(T_error)*num_levels;
+    metadata_size += sizeof(T_error)*num_levels*(num_bitplanes+1);
+    metadata_size += sizeof(SIZE)*num_levels*num_bitplanes;
+    return metadata_size;
+  }
+
+  template <typename T>
+  void Serialize(Byte * &ptr, T * data, SIZE bytes) {
+    memcpy(ptr, (Byte*)data, bytes); ptr += bytes;
+  }
+
+  template <typename T>
+  void Deserialize(Byte * &ptr, T * data, SIZE bytes) {
+    memcpy((Byte*)data, ptr, bytes); ptr += bytes;
+  }
+
+  std::vector<Byte> Serialize() {
+    std::vector<Byte> serialize_metadata(MetadataSize());
+    Byte * ptr = serialize_metadata.data();
+    Serialize(ptr, &num_levels, sizeof(SIZE));
+    Serialize(ptr, &num_bitplanes, sizeof(SIZE));
+    Serialize(ptr, level_error_bounds.data(), sizeof(T_error)*num_levels);
+    for (int i = 0; i < num_levels; i++) {
+      Serialize(ptr, level_squared_errors[i].data(), sizeof(T_error)*(num_bitplanes+1));
+    }
+    for (int i = 0; i < num_levels; i++) {
+      Serialize(ptr, level_sizes[i].data(), sizeof(SIZE)*(num_bitplanes));
+    }
+    return serialize_metadata;
+  }
+
+  void Deserialize(std::vector<Byte> serialize_metadata) {
+    Byte * ptr = serialize_metadata.data();
+    Deserialize(ptr, &num_levels, sizeof(SIZE));
+    Deserialize(ptr, &num_bitplanes, sizeof(SIZE));
+    Initialize(num_levels, num_bitplanes);
+    Deserialize(ptr, level_error_bounds.data(), sizeof(T_error)*num_levels);
+    for (int i = 0; i < num_levels; i++) {
+      Deserialize(ptr, level_squared_errors[i].data(), sizeof(T_error)*(num_bitplanes+1));
+    }
+    for (int i = 0; i < num_levels; i++) {
+      Deserialize(ptr, level_sizes[i].data(), sizeof(SIZE)*(num_bitplanes));
+    }
+  }
+
 
   SIZE num_levels;
   SIZE num_bitplanes;
