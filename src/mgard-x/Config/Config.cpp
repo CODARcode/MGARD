0e527279bf0f3d89499d001eb8e3c8eee3ecdf33
Jieyang Chen <cjy7117@gmail.com>
2022-12-05 16:36:17 -0500

    Add missing parameter in SYCL::SortByKey

--- src/mgard-x/Config/Config.cpp
+++ src/mgard-x/Config/Config.cpp
@@ -16,7 +16,7 @@ Config::Config() {
   domain_decomposition = domain_decomposition_type::MaxDim;
   decomposition = decomposition_type::MultiDim;
   estimate_outlier_ratio = 1.0;
-  huff_dict_size = 8192 * 8;
+  huff_dict_size = 8192;
   huff_block_size = 1024 * 20;
   lz4_block_size = 1 << 15;
   zstd_compress_level = 3;
