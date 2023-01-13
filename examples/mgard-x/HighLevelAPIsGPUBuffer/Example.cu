a34997265f6c469c94de699aafe0c2d3626c09ea
Jieyang Chen <cjy7117@gmail.com>
2023-01-12 23:06:39 -0600

    Update clang format

--- examples/mgard-x/HighLevelAPIsGPUBuffer/Example.cu
+++ examples/mgard-x/HighLevelAPIsGPUBuffer/Example.cu
@@ -12,21 +12,25 @@ int main() {
   // prepare
   std::cout << "Preparing...";
   void *in_array_cpu = malloc(n1 * n2 * n3 * sizeof(double));
-  for (int i = 0; i < n1 * n2 * n3; i++) ((double*)in_array_cpu)[i] = i;
+  for (int i = 0; i < n1 * n2 * n3; i++)
+    ((double *)in_array_cpu)[i] = i;
   double *in_array_gpu = nullptr;
-  cudaMalloc((void**)&in_array_gpu, n1 * n2 * n3 * sizeof(double));
-  cudaMemcpy(in_array_gpu, in_array_cpu, n1 * n2 * n3 * sizeof(double), cudaMemcpyDefault);
+  cudaMalloc((void **)&in_array_gpu, n1 * n2 * n3 * sizeof(double));
+  cudaMemcpy(in_array_gpu, in_array_cpu, n1 * n2 * n3 * sizeof(double),
+             cudaMemcpyDefault);
   void *compressed_array_cpu = malloc(n1 * n2 * n3 * sizeof(double) + 1e6);
   void *compressed_array_gpu = nullptr;
-  cudaMalloc((void**)&compressed_array_gpu, n1 * n2 * n3 * sizeof(double) + 1e6);
+  cudaMalloc((void **)&compressed_array_gpu,
+             n1 * n2 * n3 * sizeof(double) + 1e6);
   void *decompressed_array_cpu = malloc(n1 * n2 * n3 * sizeof(double));
   void *decompressed_array_gpu = nullptr;
-  cudaMalloc((void**)&decompressed_array_gpu, n1 * n2 * n3 * sizeof(double));
+  cudaMalloc((void **)&decompressed_array_gpu, n1 * n2 * n3 * sizeof(double));
 
   mgard_x::Config config;
   config.lossless = mgard_x::lossless_type::Huffman;
   config.dev_type = mgard_x::device_type::CUDA;
-  // config.log_level = mgard_x::log::ERR | mgard_x::log::INFO | mgard_x::log::TIME;
+  // config.log_level = mgard_x::log::ERR | mgard_x::log::INFO |
+  // mgard_x::log::TIME;
 
   size_t compressed_size = n1 * n2 * n3 * sizeof(double) + 1e6;
   //... load data into in_array_cpu
@@ -34,49 +38,53 @@ int main() {
   double tol = 0.01, s = 0;
   std::cout << "Done\n";
 
-  std::cout << "Compressing with MGARD-X High level API using CPU buffer input, CPU buffer output...";
+  std::cout << "Compressing with MGARD-X High level API using CPU buffer "
+               "input, CPU buffer output...";
   mgard_x::compress(3, mgard_x::data_type::Double, shape, tol, s,
                     mgard_x::error_bound_type::REL, in_array_cpu,
                     compressed_array_cpu, compressed_size, config, true);
   std::cout << "Done\n";
-  std::cout
-      << "Decompressing with MGARD-X High level API using CPU buffer input, CPU buffer output...";
+  std::cout << "Decompressing with MGARD-X High level API using CPU buffer "
+               "input, CPU buffer output...";
   mgard_x::decompress(compressed_array_cpu, compressed_size,
                       decompressed_array_cpu, config, true);
   std::cout << "Done\n";
 
-  std::cout << "Compressing with MGARD-X High level API using GPU buffer input, GPU buffer output...";
+  std::cout << "Compressing with MGARD-X High level API using GPU buffer "
+               "input, GPU buffer output...";
   mgard_x::compress(3, mgard_x::data_type::Double, shape, tol, s,
                     mgard_x::error_bound_type::REL, in_array_gpu,
                     compressed_array_gpu, compressed_size, config, true);
 
   std::cout << "Done\n";
-  std::cout
-      << "Decompressing with MGARD-X High level API using GPU buffer input, GPU buffer output...";
+  std::cout << "Decompressing with MGARD-X High level API using GPU buffer "
+               "input, GPU buffer output...";
   mgard_x::decompress(compressed_array_gpu, compressed_size,
                       decompressed_array_gpu, config, true);
   std::cout << "Done\n";
 
-  std::cout << "Compressing with MGARD-X High level API using GPU buffer input, CPU buffer output...";
+  std::cout << "Compressing with MGARD-X High level API using GPU buffer "
+               "input, CPU buffer output...";
   mgard_x::compress(3, mgard_x::data_type::Double, shape, tol, s,
                     mgard_x::error_bound_type::REL, in_array_gpu,
                     compressed_array_cpu, compressed_size, config, true);
 
   std::cout << "Done\n";
-  std::cout
-      << "Decompressing with MGARD-X High level API using CPU buffer input, GPU buffer output...";
+  std::cout << "Decompressing with MGARD-X High level API using CPU buffer "
+               "input, GPU buffer output...";
   mgard_x::decompress(compressed_array_cpu, compressed_size,
                       decompressed_array_gpu, config, true);
   std::cout << "Done\n";
 
-  std::cout << "Compressing with MGARD-X High level API using CPU buffer input, GPU buffer output...";
+  std::cout << "Compressing with MGARD-X High level API using CPU buffer "
+               "input, GPU buffer output...";
   mgard_x::compress(3, mgard_x::data_type::Double, shape, tol, s,
                     mgard_x::error_bound_type::REL, in_array_cpu,
                     compressed_array_gpu, compressed_size, config, true);
 
   std::cout << "Done\n";
-  std::cout
-      << "Decompressing with MGARD-X High level API using GPU buffer input, CPU buffer output...";
+  std::cout << "Decompressing with MGARD-X High level API using GPU buffer "
+               "input, CPU buffer output...";
   mgard_x::decompress(compressed_array_gpu, compressed_size,
                       decompressed_array_cpu, config, true);
   std::cout << "Done\n";
