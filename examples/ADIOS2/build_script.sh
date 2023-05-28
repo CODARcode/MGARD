36f0f18846ae651bd7279989c74a62b7a367be15
Jieyang Chen <cjy7117@gmail.com>
2023-05-27 19:38:42 -0500

    Fix LZ4 resize

--- examples/ADIOS2/build_script.sh
+++ examples/ADIOS2/build_script.sh
@@ -15,8 +15,8 @@ cusz_install_dir=/home/jieyang/dev/cuSZ/install
 cub=/usr/local/cuda/lib64/cmake/cub
 zfp_install_dir=/home/jieyang/dev/zfp/install
 
-rm -rf build
-mkdir build 
+# rm -rf build
+# mkdir build 
 cmake -S .  -B ./build \
 	-DCMAKE_CUDA_ARCHITECTURES=86\
 	-DCMAKE_PREFIX_PATH="${mgard_install_dir};${adios2_install_dir};${cusz_install_dir};${cub};${zfp_install_dir}"
