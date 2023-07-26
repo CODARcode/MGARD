1bb2df819f6bd2f2c0b2a793f9cd1b48bffce7ab
Jieyang Chen <cjy7117@gmail.com>
2023-07-26 15:55:56 -0500

    Update build script for Frontier

--- build_scripts/build_mgard_hip_frontier.sh
+++ build_scripts/build_mgard_hip_frontier.sh
@@ -9,18 +9,18 @@
 set -e
 set -x
 
-module load rocm/4.5.0
+module load rocm/5.5.1
 module load cmake
 
 ######## User Configurations ########
 # Source directory
 mgard_x_src_dir=.
 # Build directory
-build_dir=./build-hip-spock
+build_dir=./build-hip-crusher
 # Number of processors used for building
 num_build_procs=$1
 # Installtaion directory
-install_dir=./install-hip-spock
+install_dir=./install-hip-crusher
 
 export LD_LIBRARY_PATH=$(pwd)/${install_dir}/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$(pwd)/${install_dir}/lib64:$LD_LIBRARY_PATH
@@ -38,7 +38,7 @@ fi
 mkdir -p ${zstd_build_dir}
 cmake -S ${zstd_src_dir}/build/cmake -B ${zstd_build_dir}\
     -DZSTD_MULTITHREAD_SUPPORT=ON\
-		-DCMAKE_INSTALL_LIBDIR=lib\
+    -DCMAKE_INSTALL_LIBDIR=lib\
     -DCMAKE_INSTALL_PREFIX=${zstd_install_dir}
 cmake --build ${zstd_build_dir} -j ${num_build_procs}
 cmake --install ${zstd_build_dir}
@@ -62,14 +62,13 @@ cmake --install ${protobuf_build_dir}
 
 
 #build MGARD
-mgard_x_src_dir=${home_dir}
 mgard_x_build_dir=${build_dir}/mgard
 mgard_x_install_dir=${install_dir}
 mkdir -p ${mgard_x_build_dir}
 cmake -S ${mgard_x_src_dir} -B ${mgard_x_build_dir} \
     -DCMAKE_PREFIX_PATH="${zstd_install_dir}/lib/cmake/zstd;${protobuf_install_dir}"\
     -DMGARD_ENABLE_HIP=ON\
-    -DCMAKE_HIP_ARCHITECTURES="gfx908"\
+    -DCMAKE_HIP_ARCHITECTURES="gfx90a"\
     -DCMAKE_BUILD_TYPE=Release\
     -DCMAKE_INSTALL_PREFIX=${mgard_x_install_dir}
 cmake --build ${mgard_x_build_dir} -j ${num_build_procs}
